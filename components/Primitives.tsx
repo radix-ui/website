b60772d97a40577ce1fe3e90fa32d580617df1b1
Pedro Duarte <contact@peduarte.com>
2020-12-10 11:34:33 +0100

    Add Popover Basic Example

--- components/Primitives.tsx
+++ components/Primitives.tsx
@@ -5,7 +5,7 @@ export { Avatar as AvatarPrimitive } from '@interop-ui/react-avatar';
 export { Checkbox } from '@interop-ui/react-checkbox';
 export * as DialogPrimitive from '@interop-ui/react-dialog';
 export { Label as LabelPrimitive } from '@interop-ui/react-label';
-export { Popover } from '@interop-ui/react-popover';
+export { Popover as PopoverPrimitive } from '@interop-ui/react-popover';
 export { ProgressBar } from '@interop-ui/react-progress-bar';
 export { RadioGroup } from '@interop-ui/react-radio-group';
 export { Separator } from '@interop-ui/react-separator';
