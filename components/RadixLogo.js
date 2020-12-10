beea540fa973dfa386b99999312cad5007a310f7
Colm Tuite <colmtuite@gmail.com>
2020-12-10 01:18:53 +0000

    Update logo and favicon.

--- components/RadixLogo.js
+++ components/RadixLogo.js
@@ -1,12 +1,10 @@
 export const RadixLogo = () => (
-  <svg width="22" height="32" viewBox="0 0 42 62" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <rect width="20" height="20" fill="currentColor" />
-    <circle cx="32" cy="10" r="10" fill="currentColor" />
+  <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" viewBox="0 0 35 35" fill="none">
     <path
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M20 62V22C8.9543 22 0 30.9543 0 42C0 53.0457 8.9543 62 20 62Z"
+      d="M17 35C10.3726 35 5 30.0751 5 24C5 17.9249 10.3726 13 17 13L17 35Z"
       fill="currentColor"
     />
+    <rect x="5" width="12" height="12" fill="currentColor" />
+    <circle cx="24" cy="6" r="6" fill="currentColor" />
   </svg>
 );
