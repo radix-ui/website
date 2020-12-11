53140551ba2cc90572d9172c0c15a776ba437ff1
Colm Tuite <colmtuite@gmail.com>
2020-12-11 16:30:15 +0000

    Begin Slider animation

--- pages/primitives/docs/components/slider/Hero.tsx
+++ pages/primitives/docs/components/slider/Hero.tsx
@@ -1,28 +1,43 @@
 import React from 'react';
+import { Box, styled, css } from '@modulz/design-system';
 
-export const Hero = () => {
+const anim = css.keyframes({
+  '0%': { transform: 'translateX(100px)' },
+  '50%': { transform: 'translateX(150px)' },
+  '100%': { transform: 'translateX(50px)' },
+});
+
+const HeroSkeleton = () => {
   return (
-    <div
-      style={{
+    <Box
+      css={{
         height: '2px',
         backgroundColor: 'white',
         borderRadius: '9999px',
         flexShrink: 0,
-        width: '20%',
+        width: '200px',
         display: 'flex',
-        justifyContent: 'center',
       }}
     >
-      <div
-        style={{
+      <Box
+        css={{
           backgroundColor: 'white',
           borderRadius: '50%',
           flexShrink: 0,
-          width: '16px',
-          height: '16px',
-          marginTop: -7,
+          width: '24px',
+          height: '24px',
+          marginTop: -11,
+          marginLeft: -12,
+          transform: 'translateX(100px)',
+          // animation: `${anim} 1000ms infinite`,
+          animationDelay: '500ms',
         }}
-      ></div>
-    </div>
+      ></Box>
+    </Box>
   );
 };
+
+export const Hero = () => {
+  return <HeroSkeleton />;
+};
+
