dc248668f0477c0b183cb8e3df23c77c0f60027b
Colm Tuite <colmtuite@gmail.com>
2020-12-15 07:11:26 +0000

    Fix TabsHero bug.

--- components/Hero/TabsHero.tsx
+++ components/Hero/TabsHero.tsx
@@ -60,7 +60,92 @@ const click = css.keyframes({
 
 const TabsContainer = () => {
   return (
-    <Box>
+    <Box css={{ position: 'relative' }}>
+
+      <Box
+        css={{
+          position: 'absolute',
+          top: 18,
+          right: 88,
+          transform: 'translate(30px, 30px)',
+          animation: `${cursor} 6000ms infinite`,
+          animationTimingFunction: 'cubic-bezier(0.22, 1, 0.36, 1)',
+          zIndex: '1',
+        }}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="25"
+          height="25"
+          viewBox="0 0 25 25"
+          fill="none"
+        >
+          <g clip-path="url(#clip0)">
+            <g filter="url(#filter0_d)">
+              <path
+                d="M7.5 23.1865L4.79423 0.5L23.0885 14.1865L13.5442 15.6554L7.5 23.1865Z"
+                fill="black"
+              />
+              <path
+                d="M7.5 23.1865L4.79423 0.5L23.0885 14.1865L13.5442 15.6554L7.5 23.1865Z"
+                stroke="white"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </g>
+          </g>
+          <defs>
+            <filter
+              id="filter0_d"
+              x="-5.68303"
+              y="-5.68302"
+              width="31.4545"
+              height="32.5526"
+              filterUnits="userSpaceOnUse"
+              colorInterpolationFilters="sRGB"
+            >
+              <feFlood floodOpacity="0" result="BackgroundImageFix" />
+              <feColorMatrix
+                in="SourceAlpha"
+                type="matrix"
+                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+              />
+              <feOffset dy="1" />
+              <feGaussianBlur stdDeviation="1" />
+              <feColorMatrix
+                type="matrix"
+                values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+              />
+              <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow" />
+              <feBlend
+                mode="normal"
+                in="SourceGraphic"
+                in2="effect1_dropShadow"
+                result="shape"
+              />
+            </filter>
+            <clipPath id="clip0">
+              <rect width="25" height="25" fill="white" />
+            </clipPath>
+          </defs>
+        </svg>
+        <Box
+          css={{
+            position: 'absolute',
+            top: -12,
+            right: 0,
+            bottom: 0,
+            left: -12,
+            borderRadius: '50%',
+            boxShadow: '0 0 0 3px black',
+            opacity: '0',
+            transform: 'scale(.5)',
+            animation: `${click} 6000ms infinite`,
+            animationTimingFunction: 'cubic-bezier(0.22, 1, 0.36, 1)',
+          }}
+        ></Box>
+      </Box>
+
       <Flex>
         <Box
           css={{
@@ -75,7 +160,7 @@ const TabsContainer = () => {
             animation: `${tab1} 6000ms infinite`,
           }}
         >
-          <Box css={{ backgroundColor: '$gray700', height: 4, width: '100%' }}></Box>
+          <Box css={{ backgroundColor: 'hsl(206,10%,80%)', height: 4, width: '100%' }}></Box>
         </Box>
         <Box
           css={{
@@ -91,89 +176,7 @@ const TabsContainer = () => {
             animation: `${tab2} 6000ms infinite`,
           }}
         >
-          <Box css={{ backgroundColor: '$gray700', height: 4, width: '100%' }}></Box>
-          <Box
-            css={{
-              position: 'absolute',
-              bottom: -17,
-              right: -12,
-              transform: 'translate(30px, 30px)',
-              animation: `${cursor} 6000ms infinite`,
-              animationTimingFunction: 'cubic-bezier(0.22, 1, 0.36, 1)',
-            }}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="25"
-              height="25"
-              viewBox="0 0 25 25"
-              fill="none"
-            >
-              <g clip-path="url(#clip0)">
-                <g filter="url(#filter0_d)">
-                  <path
-                    d="M7.5 23.1865L4.79423 0.5L23.0885 14.1865L13.5442 15.6554L7.5 23.1865Z"
-                    fill="black"
-                  />
-                  <path
-                    d="M7.5 23.1865L4.79423 0.5L23.0885 14.1865L13.5442 15.6554L7.5 23.1865Z"
-                    stroke="white"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </g>
-              </g>
-              <defs>
-                <filter
-                  id="filter0_d"
-                  x="-5.68303"
-                  y="-5.68302"
-                  width="31.4545"
-                  height="32.5526"
-                  filterUnits="userSpaceOnUse"
-                  colorInterpolationFilters="sRGB"
-                >
-                  <feFlood floodOpacity="0" result="BackgroundImageFix" />
-                  <feColorMatrix
-                    in="SourceAlpha"
-                    type="matrix"
-                    values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-                  />
-                  <feOffset dy="1" />
-                  <feGaussianBlur stdDeviation="1" />
-                  <feColorMatrix
-                    type="matrix"
-                    values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
-                  />
-                  <feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow" />
-                  <feBlend
-                    mode="normal"
-                    in="SourceGraphic"
-                    in2="effect1_dropShadow"
-                    result="shape"
-                  />
-                </filter>
-                <clipPath id="clip0">
-                  <rect width="25" height="25" fill="white" />
-                </clipPath>
-              </defs>
-            </svg>
-            <Box
-              css={{
-                position: 'absolute',
-                top: -12,
-                right: 0,
-                bottom: 0,
-                left: -12,
-                borderRadius: '50%',
-                boxShadow: '0 0 0 3px black',
-                opacity: '0',
-                transform: 'scale(.5)',
-                animation: `${click} 6000ms infinite`,
-                animationTimingFunction: 'cubic-bezier(0.22, 1, 0.36, 1)',
-              }}
-            ></Box>
-          </Box>
+          <Box css={{ backgroundColor: 'hsl(206,10%,80%)', height: 4, width: '100%' }}></Box>
         </Box>
       </Flex>
       <Box
@@ -186,13 +189,13 @@ const TabsContainer = () => {
           left: '50%',
         }}
       >
-        <Box css={{ backgroundColor: '$gray700', height: 12, width: '35%', mb: 12 }}></Box>
-        <Box css={{ backgroundColor: '$gray700', height: 4, width: '100%', mb: 12 }}></Box>
-        <Box css={{ backgroundColor: '$gray700', height: 4, width: '50%', mb: 12 }}></Box>
+        <Box css={{ backgroundColor: 'hsl(206,10%,80%)', height: 12, width: '35%', mb: 12 }}></Box>
+        <Box css={{ backgroundColor: 'hsl(206,10%,80%)', height: 4, width: '100%', mb: 12 }}></Box>
+        <Box css={{ backgroundColor: 'hsl(206,10%,80%)', height: 4, width: '50%', mb: 12 }}></Box>
         <Box
           css={{
             animation: `${text} 6000ms infinite`,
-            backgroundColor: '$gray700',
+            backgroundColor: 'hsl(206,10%,80%)',
             height: 4,
             width: '75%',
             mb: 12,
@@ -201,7 +204,7 @@ const TabsContainer = () => {
         <Box
           css={{
             animation: `${text} 6000ms infinite`,
-            backgroundColor: '$gray700',
+            backgroundColor: 'hsl(206,10%,80%)',
             height: 4,
             width: '50%',
           }}
