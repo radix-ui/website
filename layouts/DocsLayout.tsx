b18d834135418b6c809cd3a8187418c01c2dbe2b
Chance Strickland <chance@modulz.app>
2020-12-16 12:31:07 -0800

    Fix scrollbar jank on QuickNav (#64) (#68)

--- layouts/DocsLayout.tsx
+++ layouts/DocsLayout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import NextLink from 'next/link';
 import { useRouter } from 'next/router';
+import { RemoveScroll } from 'react-remove-scroll';
 import { MDXProvider } from '@mdx-js/react';
 import {
   Text,
@@ -63,6 +64,14 @@ export default function DocsLayout({ children, frontMatter }: LayoutProps) {
       <ScrollArea>
         <Box
           as="aside"
+          // Components that hide the scrollbar (like Dialog) add padding to
+          // account for the scrollbar gap to avoid layout jank. This does not
+          // work for position: fixed elements. Since we use react-remove-scroll
+          // under the hood for those primitives, we can add this helper class
+          // provided by that lib to deal with that for the QuickNav.
+          // https://github.com/radix-ui/website/issues/64
+          // https://github.com/theKashey/react-remove-scroll#positionfixed-elements
+          className={RemoveScroll.classNames.zeroRight}
           css={{
             display: 'none',
             bp3: {
