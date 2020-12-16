07e6a5dde1fdcb4d00679f1accf2dbb186bbcb24
Chance Strickland <hi@chancedigital.io>
2020-12-16 13:53:42 -0800

    Patch fix for position: fixed jank in theme toggle (#71)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { AppProps } from 'next/app';
-import Head from 'next/head';
+import { RemoveScroll } from 'react-remove-scroll';
 import { useRouter } from 'next/router';
 import useDarkMode from 'use-dark-mode';
-import { Box, darkThemeClass } from '@modulz/design-system';
+import { Box, darkThemeClass, theme as DSTheme, styled } from '@modulz/design-system';
 import { Footer } from '../components/Footer';
 import { ThemeToggle } from '../components/ThemeToggle';
 import { DocsPage } from '../components/DocsPage';
 import { BlogPage } from '../components/BlogPage';
 import { useAnalytics } from '../utils/analytics';
 import { scrollToUrlHash } from '../utils/scrollToUrlHash';
+import { useMatchMedia } from '../utils/useMatchMedia';
 
 function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -39,6 +40,12 @@ function App({ Component, pageProps }: AppProps) {
   const isDocs = router.pathname.includes('/docs');
   const isBlog = router.pathname.includes('/blog/');
 
+  // https://github.com/radix-ui/website/issues/71
+  // Breakpoint value not exposed directly, got it from https://github.com/modulz/design-system/blob/master/stitches.config.ts#L420
+  // TODO: Remove this when we come up with a better solution for react-remove-scroll in primitives
+  const isBp2 = useMatchMedia('screen and (min-width: 900px)');
+  const positionFixedPatchClassName = isBp2 ? RemoveScroll.classNames.zeroRight : undefined;
+
   // Dark theme hack to prevent flash
   // prevents ssr flash for mismatched dark mode
   // https://brianlovin.com/overthought/adding-dark-mode-with-next-js
@@ -53,6 +60,7 @@ function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Box
+        className={positionFixedPatchClassName}
         css={{
           position: 'absolute',
           top: '$5',
@@ -61,7 +69,8 @@ function App({ Component, pageProps }: AppProps) {
           bp2: {
             position: 'fixed',
             top: '$3',
-            right: '$3',
+            right: 0,
+            paddingRight: '$3',
           },
         }}
       >
