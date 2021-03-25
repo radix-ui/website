f3a6eeef0b87fa96b6c2aa3277a459c686408a1a
Benoît Grélard <benoit.grelard@gmail.com>
2021-03-25 10:30:04 +0000

    Update docs - part 1

--- components/Primitives.tsx
+++ components/Primitives.tsx
@@ -20,7 +20,7 @@ export * as Separator from '@radix-ui/react-separator';
 export * as Slider from '@radix-ui/react-slider';
 export * as Switch from '@radix-ui/react-switch';
 export * as Tabs from '@radix-ui/react-tabs';
-export * as ToggleButton from '@radix-ui/react-toggle-button';
+export * as Toggle from '@radix-ui/react-toggle';
 export * as Tooltip from '@radix-ui/react-tooltip';
 export * as Slot from '@radix-ui/react-slot';
 export * as VisuallyHidden from '@radix-ui/react-visually-hidden';
