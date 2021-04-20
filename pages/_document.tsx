556284fe875445569953339d7e31b3445ac73576
Pedro Duarte <contact@peduarte.com>
2021-04-20 19:01:46 +0200

    Add preload to font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,12 @@ export default class Document extends NextDocument {
           <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssString() }} />
           <script dangerouslySetInnerHTML={{ __html: renderSnippet() }} />
           <link rel="icon" href="/favicon.png" />
-          <link rel="stylesheet" href="https://develop.modulz.app/fonts/fonts.css" />
+          <link
+            href="https://develop.modulz.app/fonts/fonts.css"
+            rel="preload"
+            as="font"
+            crossOrigin="anonymous"
+          />
         </Head>
         <body>
           <Main />
