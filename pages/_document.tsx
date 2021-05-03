ed04c37a9a2e6eb5c001f6200b41fabba98cf98f
Pedro Duarte <contact@peduarte.com>
2021-05-03 13:50:26 +0200

    Load fonts locally (#144)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,10 +10,73 @@ export default class Document extends NextDocument {
         <Head>
           <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssString() }} />
           <link
-            href="https://develop.modulz.app/fonts/fonts.css"
             rel="preload"
+            href="/fonts/UntitledSansWeb-Regular.woff"
             as="font"
+            type="font/woff"
             crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/UntitledSansWeb-Regular.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/UntitledSansWeb-Medium.woff"
+            as="font"
+            type="font/woff"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/UntitledSansWeb-Medium.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/soehne-mono-web-buch.woff"
+            as="font"
+            type="font/woff"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preload"
+            href="/fonts/soehne-mono-web-buch.woff2"
+            as="font"
+            type="font/woff2"
+            crossOrigin="anonymous"
+          />
+          <style
+            dangerouslySetInnerHTML={{
+              __html: `
+@font-face {
+  font-family: 'Untitled Sans';
+  font-weight: 400;
+  font-display: swap;
+  src: url(/fonts/UntitledSansWeb-Regular.woff2) format('woff2'), url(/fonts/UntitledSansWeb-Regular.woff) format('woff');
+}
+
+@font-face {
+  font-family: 'Untitled Sans';
+  font-weight: 500;
+  font-display: swap;
+  src: url(/fonts/UntitledSansWeb-Medium.woff2) format('woff2'), url(/fonts/UntitledSansWeb-Medium.woff) format('woff');
+}
+
+@font-face {
+  font-family: 'Söhne Mono';
+  font-weight: normal;
+  font-style: normal;
+  font-display: swap;
+  src: url('/fonts/soehne-mono-web-buch.woff2') format('woff2'), url('/fonts/soehne-mono-web-buch.woff') format('woff');
+}
+`,
+            }}
           />
           <link rel="icon" href="/favicon.png" />
           <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
