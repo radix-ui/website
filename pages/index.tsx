56a2b99c854cbf929d11f8f3603784f095f0d96f
Colm Tuite <colmtuite@gmail.com>
2021-05-22 01:13:21 +0100

    Add links to homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
-import { Section, Container, Heading, Paragraph, Box, Grid, Text } from '@modulz/design-system';
+import NextLink from 'next/link';
+import {
+  Section,
+  Link,
+  Container,
+  Heading,
+  Paragraph,
+  Box,
+  Grid,
+  Text,
+} from '@modulz/design-system';
 
 export default function Home() {
-  return <Box>oh hi hello</Box>;
+  return (
+    <Box>
+      oh hi hello
+      <NextLink href="/primitives/docs/overview/introduction" passHref>
+        <Link variant="blue">Primitives</Link>
+      </NextLink>
+      <NextLink href="/colors" passHref>
+        <Link variant="blue">Colors</Link>
+      </NextLink>
+    </Box>
+  );
 }
