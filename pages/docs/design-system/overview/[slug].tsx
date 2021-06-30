221257c6bd87e94adf0a912e4e8e7fb11a2348f2
Colm Tuite <colmtuite@gmail.com>
2021-06-30 07:10:28 +0100

    Fix sidebar

--- pages/docs/design-system/overview/[slug].tsx
+++ pages/docs/design-system/overview/[slug].tsx
@@ -47,7 +47,7 @@ export default function DesignSystemOverviewDoc({ frontmatter, code }: Doc) {
             flexShrink: 0,
             zIndex: 1,
             position: 'fixed',
-            top: 0,
+            top: '$sizes$7',
             right: 0,
             bottom: 0,
           },
