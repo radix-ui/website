a1dfeb27358b992179dfd6f9380aa288db3de1fe
Pedro Duarte <contact@peduarte.com>
2021-07-01 15:03:52 +0200

    Upgrade mdx-bundler

--- lib/mdx.ts
+++ lib/mdx.ts
@@ -32,7 +32,7 @@ export const getAllFrontmatter = (fromPath) => {
 export const getMdxBySlug = async (basePath, slug) => {
   const source = fs.readFileSync(path.join(DATA_PATH, basePath, `${slug}.mdx`), 'utf8');
   const { frontmatter, code } = await bundleMDX(source, {
-    xdmOptions(input, options) {
+    xdmOptions(options) {
       options.remarkPlugins = [...(options.remarkPlugins ?? []), remarkSlug];
       options.rehypePlugins = [
         ...(options.rehypePlugins ?? []),
