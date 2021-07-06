294f9fb63bbe436284ba0fe477094189601cefd1
Pedro Duarte <contact@peduarte.com>
2021-07-06 08:56:28 +0200

    Remove hero from other sub versions

--- components/OldVersionNote.tsx
+++ components/OldVersionNote.tsx
@@ -12,8 +12,8 @@ export function OldVersionNote({ name, href }) {
         mx: '-$5',
         py: '$2',
         px: '$5',
-        bc: '$yellow100',
-        boxShadow: '0 0 0 1px $colors$yellow400',
+        bc: '$yellow2',
+        boxShadow: '0 0 0 1px $colors$yellow5',
 
         '@bp2': {
           position: 'absolute',
