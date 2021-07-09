9fe965d3c9ba93c5a272b373a3cec5ebfa24d107
Pedro Duarte <contact@peduarte.com>
2021-07-09 17:54:39 +0200

    Create DropdownMenu.jsx

---
+++ components/demos/DropdownMenu.jsx
@@ -0,0 +1,221 @@
+import React from 'react';
+import { styled, keyframes } from '@modulz/design-system';
+import { violet, mauve, amber, lime, mint, blackA } from '@radix-ui/colors';
+import {
+  HamburgerMenuIcon,
+  DotFilledIcon,
+  CheckIcon,
+  ChevronRightIcon,
+} from '@radix-ui/react-icons';
+import * as DropdownMenuPrimitive from '@radix-ui/react-dropdown-menu';
+
+const StyledContent = styled(DropdownMenuPrimitive.Content, {
+  minWidth: 220,
+  backgroundColor: violet.violet1,
+  borderRadius: 6,
+  overflow: 'hidden',
+  padding: 5,
+  boxShadow:
+    '0px 10px 38px -10px rgba(22, 23, 24, 0.35), 0px 10px 20px -15px rgba(22, 23, 24, 0.2)',
+});
+
+const itemStyles = {
+  all: 'unset',
+  fontSize: 13,
+  lineHeight: 1,
+  color: violet.violet11,
+  borderRadius: 4,
+  display: 'flex',
+  alignItems: 'center',
+  height: 25,
+  padding: '0 5px',
+  position: 'relative',
+  paddingLeft: 25,
+  userSelect: 'none',
+
+  '&[data-disabled]': {
+    color: mauve.mauve8,
+    pointerEvents: 'none',
+  },
+
+  '&:hover, &:focus': {
+    backgroundColor: violet.violet9,
+    color: violet.violet1,
+  },
+};
+
+const StyledItem = styled(DropdownMenuPrimitive.Item, { ...itemStyles });
+const StyledCheckboxItem = styled(DropdownMenuPrimitive.CheckboxItem, { ...itemStyles });
+const StyledRadioItem = styled(DropdownMenuPrimitive.RadioItem, { ...itemStyles });
+const StyledTriggerItem = styled(DropdownMenuPrimitive.TriggerItem, {
+  '&[data-state="open"]': {
+    backgroundColor: violet.violet4,
+    color: violet.violet11,
+  },
+  ...itemStyles,
+});
+
+const StyledLabel = styled(DropdownMenuPrimitive.Label, {
+  paddingLeft: 25,
+  fontSize: 11,
+  fontWeight: 500,
+  lineHeight: '25px',
+  color: violet.violet8,
+});
+
+const StyledSeparator = styled(DropdownMenuPrimitive.Separator, {
+  height: 1,
+  backgroundColor: violet.violet6,
+  margin: 5,
+});
+
+const StyledItemIndicator = styled(DropdownMenuPrimitive.ItemIndicator, {
+  position: 'absolute',
+  left: 0,
+  width: 25,
+  display: 'inline-flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+});
+
+// Exports
+export const DropdownMenu = DropdownMenuPrimitive.Root;
+export const DropdownMenuTrigger = DropdownMenuPrimitive.Trigger;
+export const DropdownMenuContent = StyledContent;
+export const DropdownMenuItem = StyledItem;
+export const DropdownMenuCheckboxItem = StyledCheckboxItem;
+export const DropdownMenuRadioGroup = DropdownMenuPrimitive.RadioGroup;
+export const DropdownMenuRadioItem = StyledRadioItem;
+export const DropdownMenuItemIndicator = StyledItemIndicator;
+export const DropdownMenuTriggerItem = StyledTriggerItem;
+export const DropdownMenuLabel = StyledLabel;
+export const DropdownMenuSeparator = StyledSeparator;
+
+// Your app...
+const Box = styled('div', {});
+
+const RightSlot = styled('div', {
+  marginLeft: 'auto',
+  paddingLeft: 20,
+  color: violet.violet8,
+});
+
+const IconButton = styled('button', {
+  all: 'unset',
+  fontFamily: 'inherit',
+  borderRadius: '100%',
+  height: 35,
+  width: 35,
+  display: 'inline-flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  color: 'white',
+  marginLeft: 20,
+  backgroundColor: blackA.blackA10,
+
+  '&:hover': { backgroundColor: blackA.blackA11 },
+  '&:focus': { boxShadow: `0 0 0 2px ${violet.violet7}` },
+});
+
+export const DropdownMenuDemo = () => {
+  const [bookmarksChecked, setBookmarksChecked] = React.useState(true);
+  const [urlsChecked, setUrlsChecked] = React.useState(false);
+  const [person, setPerson] = React.useState('pedro');
+
+  return (
+    <Box>
+      <DropdownMenu>
+        <DropdownMenuTrigger as={IconButton}>
+          <HamburgerMenuIcon />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent sideOffset={5} align="end">
+          <DropdownMenuItem>
+            New Tab <RightSlot>⌘+T</RightSlot>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            New Window <RightSlot>⌘+N</RightSlot>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            New Private Window <RightSlot>⇧+⌘+N</RightSlot>
+          </DropdownMenuItem>
+          <DropdownMenu>
+            <DropdownMenuTriggerItem>
+              History{' '}
+              <RightSlot>
+                <ChevronRightIcon />
+              </RightSlot>
+            </DropdownMenuTriggerItem>
+            <DropdownMenuContent sideOffset={2} alignOffset={-5}>
+              <DropdownMenuItem>Stitches</DropdownMenuItem>
+              <DropdownMenuItem>Radix</DropdownMenuItem>
+              <DropdownMenuItem>Modulz</DropdownMenuItem>
+              <DropdownMenuItem>Twitter</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <DropdownMenuSeparator />
+          <DropdownMenuLabel>Edit</DropdownMenuLabel>
+          <DropdownMenuItem>Cut</DropdownMenuItem>
+          <DropdownMenuItem>Copy</DropdownMenuItem>
+          <DropdownMenuItem>Paste</DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            Print… <RightSlot>⌘+P</RightSlot>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            Find… <RightSlot>⌘+F</RightSlot>
+          </DropdownMenuItem>
+          <DropdownMenu>
+            <DropdownMenuTriggerItem>
+              More Tools
+              <RightSlot>
+                <ChevronRightIcon />
+              </RightSlot>
+            </DropdownMenuTriggerItem>
+            <DropdownMenuContent sideOffset={2} alignOffset={-5}>
+              <DropdownMenuItem>
+                Save Page As… <RightSlot>⌘+S</RightSlot>
+              </DropdownMenuItem>
+              <DropdownMenuItem>Create RightSlot… </DropdownMenuItem>
+              <DropdownMenuItem>Name Window… </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Developer Tools</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+          <DropdownMenuSeparator />
+          <DropdownMenuCheckboxItem
+            checked={bookmarksChecked}
+            onCheckedChange={setBookmarksChecked}
+          >
+            <DropdownMenuItemIndicator>
+              <CheckIcon />
+            </DropdownMenuItemIndicator>
+            Show Bookmarks <RightSlot>⌘+B</RightSlot>
+          </DropdownMenuCheckboxItem>
+          <DropdownMenuCheckboxItem checked={urlsChecked} onCheckedChange={setUrlsChecked}>
+            <DropdownMenuItemIndicator>
+              <CheckIcon />
+            </DropdownMenuItemIndicator>
+            Show Full URLs
+          </DropdownMenuCheckboxItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuRadioGroup value={person} onValueChange={setPerson}>
+            <DropdownMenuRadioItem value="pedro">
+              <DropdownMenuItemIndicator>
+                <DotFilledIcon />
+              </DropdownMenuItemIndicator>
+              Pedro Duarte
+            </DropdownMenuRadioItem>
+            <DropdownMenuRadioItem value="colm">
+              <DropdownMenuItemIndicator>
+                <DotFilledIcon />
+              </DropdownMenuItemIndicator>
+              Colm Tuite
+            </DropdownMenuRadioItem>
+          </DropdownMenuRadioGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </Box>
+  );
+};
+
+export default DropdownMenuDemo;
