abc9e8c42e7596bb4b872a8b8892dca4b1452502
Pedro Duarte <contact@peduarte.com>
2021-07-14 11:27:34 +0200

    Tabs

--- components/demos/index.jsx
+++ components/demos/index.jsx
@@ -15,4 +15,5 @@ export { default as ScrollAreaDemo } from './ScrollArea';
 export { default as SeparatorDemo } from './Separator';
 export { default as SliderDemo } from './Slider';
 export { default as SwitchDemo } from './Switch';
+export { default as TabsDemo } from './Tabs';
 export { default as TooltipDemo } from './Tooltip';
