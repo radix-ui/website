f2e9ab114019edc0949c3601d2b92cd0d1684b6e
Pedro Duarte <contact@peduarte.com>
2021-07-15 09:25:01 +0200

    Update Popover.jsx

--- components/demos/Popover.jsx
+++ components/demos/Popover.jsx
@@ -125,20 +125,20 @@ const PopoverDemo = () => (
           Dimensions
         </Text>
         <Fieldset>
-          <Label>Width</Label>
-          <Input value="100%" />
+          <Label htmlFor="width">Width</Label>
+          <Input id="width" defaultValue="100%" />
         </Fieldset>
         <Fieldset>
-          <Label>Max. width</Label>
-          <Input value="300px" />
+          <Label htmlFor="maxWidth">Max. width</Label>
+          <Input id="maxWidth" defaultValue="300px" />
         </Fieldset>
         <Fieldset>
-          <Label>Height</Label>
-          <Input value="25px" />
+          <Label htmlFor="height">Height</Label>
+          <Input id="height" defaultValue="25px" />
         </Fieldset>
         <Fieldset>
-          <Label>Max. height</Label>
-          <Input value="none" />
+          <Label htmlFor="maxHeight">Max. height</Label>
+          <Input id="maxHeight" defaultValue="none" />
         </Fieldset>
       </Flex>
       <PopoverArrow />
