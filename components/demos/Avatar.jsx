03b02fc0404e07b203bbe77fdf4def3c849e467d
Pedro Duarte <contact@peduarte.com>
2021-07-15 09:14:50 +0200

    Update Avatar.jsx

--- components/demos/Avatar.jsx
+++ components/demos/Avatar.jsx
@@ -47,15 +47,21 @@ const Flex = styled('div', { display: 'flex' });
 const AvatarDemo = () => (
   <Flex css={{ gap: 20 }}>
     <Avatar>
-      <AvatarImage src="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=128&h=128&dpr=2&q=80" />
-      <AvatarFallback delayMs={600}>PD</AvatarFallback>
+      <AvatarImage
+        src="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
+        alt="Colm Tuite"
+      />
+      <AvatarFallback delayMs={600}>CT</AvatarFallback>
     </Avatar>
     <Avatar>
-      <AvatarImage src="https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=3&w=128&h=128&dpr=2&q=80" />
-      <AvatarFallback delayMs={600}>PD</AvatarFallback>
+      <AvatarImage
+        src="https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&w=128&h=128&dpr=2&q=80"
+        alt="Pedro Duarte"
+      />
+      <AvatarFallback delayMs={600}>JD</AvatarFallback>
     </Avatar>
     <Avatar>
-      <AvatarFallback>CT</AvatarFallback>
+      <AvatarFallback>PD</AvatarFallback>
     </Avatar>
   </Flex>
 );
