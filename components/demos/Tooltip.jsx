0d6689b13a05e51281169f37bdff5a54b3082cf5
Jenna Smith <jenna@modulz.app>
2021-12-10 20:14:16 +0000

    [Release] Remove redunant changelog item (#305)

--- components/demos/Tooltip.jsx
+++ components/demos/Tooltip.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { styled, keyframes, theme } from '@modulz/design-system';
-import { PlusIcon, MinusIcon } from '@radix-ui/react-icons';
+import { PlusIcon } from '@radix-ui/react-icons';
 import { violet, blackA } from '@radix-ui/colors';
 import * as TooltipPrimitive from '@radix-ui/react-tooltip';
 
