607c7b33267a0bf9956d48d7d7c63f513ed9473b
Andy Hook <hello@andy-hook.co.uk>
2022-02-21 11:31:25 +0000

    Initial `NavigationMenu` docs

---
+++ components/demos/NavigationMenu.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { styled } from '@modulz/design-system';
+
+// Your website...
+const Box = styled('div', {});
+
+export const NavigationMenuDemo = () => {
+  return <Box>NavigationMenu</Box>;
+};
+
+export default NavigationMenuDemo;
