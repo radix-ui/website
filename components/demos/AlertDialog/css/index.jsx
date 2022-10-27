b5cbd7da34d61a9be11e6889a6b8c76f5e260beb
Andy Hook <hello@andyhook.dev>
2022-10-27 12:40:13 +0100

    AlertDialogButton -> Button

--- components/demos/AlertDialog/css/index.jsx
+++ components/demos/AlertDialog/css/index.jsx
@@ -5,7 +5,7 @@ import './styles.css';
 const AlertDialogDemo = () => (
   <AlertDialog.Root>
     <AlertDialog.Trigger asChild>
-      <button className="AlertDialogButton violet">Delete account</button>
+      <button className="Button violet">Delete account</button>
     </AlertDialog.Trigger>
     <AlertDialog.Portal>
       <AlertDialog.Overlay className="AlertDialogOverlay" />
@@ -17,10 +17,10 @@ const AlertDialogDemo = () => (
         </AlertDialog.Description>
         <div style={{ display: 'flex', gap: 25, justifyContent: 'flex-end' }}>
           <AlertDialog.Cancel asChild>
-            <button className="AlertDialogButton mauve">Cancel</button>
+            <button className="Button mauve">Cancel</button>
           </AlertDialog.Cancel>
           <AlertDialog.Action asChild>
-            <button className="AlertDialogButton red">Yes, delete account</button>
+            <button className="Button red">Yes, delete account</button>
           </AlertDialog.Action>
         </div>
       </AlertDialog.Content>
