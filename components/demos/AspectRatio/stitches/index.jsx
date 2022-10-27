1bffd8c649ed8ce3dce89c43eae62361da80f1b6
Andy Hook <hello@andyhook.dev>
2022-10-27 13:37:54 +0100

    Add vanilla CSS support to AspectRatio (#508)
    * Adjust and add demos
    
    * photograph alt text

--- components/demos/AspectRatio/stitches/index.jsx
+++ components/demos/AspectRatio/stitches/index.jsx
@@ -1,18 +1,7 @@
 import React from 'react';
-import { styled } from '@modulz/design-system';
+import * as AspectRatio from '@radix-ui/react-aspect-ratio';
+import { styled } from '@stitches/react';
 import { blackA } from '@radix-ui/colors';
-import * as AspectRatioPrimitive from '@radix-ui/react-aspect-ratio';
-
-// Exports
-export const AspectRatio = AspectRatioPrimitive;
-
-// Your app...
-const Box = styled('div', {});
-const Img = styled('img', {
-  objectFit: 'cover',
-  width: '100%',
-  height: '100%',
-});
 
 const AspectRatioDemo = () => (
   <Box
@@ -26,10 +15,17 @@ const AspectRatioDemo = () => (
     <AspectRatio.Root ratio={16 / 9}>
       <Img
         src="https://images.unsplash.com/photo-1535025183041-0991a977e25b?w=300&dpr=2&q=80"
-        alt="Landscape photo by Tobias Tullius"
+        alt="Landscape photograph by Tobias Tullius"
       />
     </AspectRatio.Root>
   </Box>
 );
+
+const Box = styled('div', {});
+const Img = styled('img', {
+  objectFit: 'cover',
+  width: '100%',
+  height: '100%',
+});
 
 export default AspectRatioDemo;
