29ff960c282f0432d0bdd0c6c69d7b246f7d158b
Andy Hook <hello@andyhook.dev>
2022-10-31 08:45:02 +0000

    Add CSS demo (#511)

--- components/demos/index.jsx
+++ components/demos/index.jsx
@@ -2,7 +2,7 @@ export { GettingStartedDemo } from './GettingStarted';
 export { default as AccordionDemo } from './Accordion/stitches';
 export { default as AlertDialogDemo } from './AlertDialog/stitches';
 export { default as AspectRatioDemo } from './AspectRatio/stitches';
-export { default as AvatarDemo } from './Avatar';
+export { default as AvatarDemo } from './Avatar/stitches';
 export { default as CheckboxDemo } from './Checkbox';
 export { default as CollapsibleDemo } from './Collapsible';
 export { default as ContextMenuDemo } from './ContextMenu';
