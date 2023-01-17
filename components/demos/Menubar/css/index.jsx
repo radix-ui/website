cbfc82bc0710907a8a90d36e5c6e2ba10a01ad87
Benoît Grélard <benoit.grelard@gmail.com>
2023-01-17 14:12:12 +0000

    January 17 release (#565)
    * Base commit
    
    * Add release notes
    
    * Rename files with patches and no docs changes
    
    * Select docs (#566)
    
    * Duplicate file and remove hero code from previous version
    
    * Add new props and parts
    
    * Update some props
    
    * Add new examples
    
    * Apply suggestions from code review
    
    Co-authored-by: Andy Hook <hello@andyhook.dev>
    
    * Invert not on props availability
    
    Co-authored-by: Andy Hook <hello@andyhook.dev>
    
    * Add `MenuBar` docs (#567)
    
    * Add Menubar documentation
    
    * Add css demo
    
    * Fix janky demo
    
    Co-authored-by: Andy Hook <hello@andyhook.dev>

---
+++ components/demos/Menubar/css/index.jsx
@@ -0,0 +1,162 @@
+import React from 'react';
+import * as Menubar from '@radix-ui/react-menubar';
+import { CheckIcon, ChevronRightIcon, DotFilledIcon } from '@radix-ui/react-icons';
+import './styles.css';
+
+const RADIO_ITEMS = ['Andy', 'Benoît', 'Luis'];
+const CHECK_ITEMS = ['Always Show Bookmarks Bar', 'Always Show Full URLs'];
+
+const MenubarDemo = () => {
+  const [checkedSelection, setCheckedSelection] = React.useState([CHECK_ITEMS[1]]);
+  const [radioSelection, setRadioSelection] = React.useState(RADIO_ITEMS[2]);
+
+  return (
+    <Menubar.Root className="MenubarRoot">
+      <Menubar.Menu>
+        <Menubar.Trigger className="MenubarTrigger">File</Menubar.Trigger>
+        <Menubar.Portal>
+          <Menubar.Content className="MenubarContent" align="start" sideOffset={5} alignOffset={-3}>
+            <Menubar.Item className="MenubarItem">
+              New Tab <div className="RightSlot">⌘ T</div>
+            </Menubar.Item>
+            <Menubar.Item className="MenubarItem">
+              New Window <div className="RightSlot">⌘ N</div>
+            </Menubar.Item>
+            <Menubar.Item className="MenubarItem" disabled>
+              New Incognito Window
+            </Menubar.Item>
+            <Menubar.Separator className="MenubarSeparator" />
+            <Menubar.Sub>
+              <Menubar.SubTrigger className="MenubarSubTrigger">
+                Share
+                <div className="RightSlot">
+                  <ChevronRightIcon />
+                </div>
+              </Menubar.SubTrigger>
+              <Menubar.Portal>
+                <Menubar.SubContent className="MenubarSubContent" alignOffset={-5}>
+                  <Menubar.Item className="MenubarItem">Email Link</Menubar.Item>
+                  <Menubar.Item className="MenubarItem">Messages</Menubar.Item>
+                  <Menubar.Item className="MenubarItem">Notes</Menubar.Item>
+                </Menubar.SubContent>
+              </Menubar.Portal>
+            </Menubar.Sub>
+            <Menubar.Separator className="MenubarSeparator" />
+            <Menubar.Item className="MenubarItem">
+              Print… <div className="RightSlot">⌘ P</div>
+            </Menubar.Item>
+          </Menubar.Content>
+        </Menubar.Portal>
+      </Menubar.Menu>
+
+      <Menubar.Menu>
+        <Menubar.Trigger className="MenubarTrigger">Edit</Menubar.Trigger>
+        <Menubar.Portal>
+          <Menubar.Content className="MenubarContent" align="start" sideOffset={5} alignOffset={-3}>
+            <Menubar.Item className="MenubarItem">
+              Undo <div className="RightSlot">⌘ Z</div>
+            </Menubar.Item>
+            <Menubar.Item className="MenubarItem">
+              Redo <div className="RightSlot">⇧ ⌘ Z</div>
+            </Menubar.Item>
+            <Menubar.Separator className="MenubarSeparator" />
+            <Menubar.Sub>
+              <Menubar.SubTrigger className="MenubarSubTrigger">
+                Find
+                <div className="RightSlot">
+                  <ChevronRightIcon />
+                </div>
+              </Menubar.SubTrigger>
+
+              <Menubar.Portal>
+                <Menubar.SubContent className="MenubarSubContent" alignOffset={-5}>
+                  <Menubar.Item className="MenubarItem">Search the web…</Menubar.Item>
+                  <Menubar.Separator className="MenubarSeparator" />
+                  <Menubar.Item className="MenubarItem">Find…</Menubar.Item>
+                  <Menubar.Item className="MenubarItem">Find Next</Menubar.Item>
+                  <Menubar.Item className="MenubarItem">Find Previous</Menubar.Item>
+                </Menubar.SubContent>
+              </Menubar.Portal>
+            </Menubar.Sub>
+            <Menubar.Separator className="MenubarSeparator" />
+            <Menubar.Item className="MenubarItem">Cut</Menubar.Item>
+            <Menubar.Item className="MenubarItem">Copy</Menubar.Item>
+            <Menubar.Item className="MenubarItem">Paste</Menubar.Item>
+          </Menubar.Content>
+        </Menubar.Portal>
+      </Menubar.Menu>
+
+      <Menubar.Menu>
+        <Menubar.Trigger className="MenubarTrigger">View</Menubar.Trigger>
+        <Menubar.Portal>
+          <Menubar.Content
+            className="MenubarContent"
+            align="start"
+            sideOffset={5}
+            alignOffset={-14}
+          >
+            {CHECK_ITEMS.map((item) => (
+              <Menubar.CheckboxItem
+                className="MenubarCheckboxItem inset"
+                key={item}
+                checked={checkedSelection.includes(item)}
+                onCheckedChange={() =>
+                  setCheckedSelection((current) =>
+                    current.includes(item)
+                      ? current.filter((el) => el !== item)
+                      : current.concat(item)
+                  )
+                }
+              >
+                <Menubar.ItemIndicator className="MenubarItemIndicator">
+                  <CheckIcon />
+                </Menubar.ItemIndicator>
+                {item}
+              </Menubar.CheckboxItem>
+            ))}
+            <Menubar.Separator className="MenubarSeparator" />
+            <Menubar.Item className="MenubarItem inset">
+              Reload <div className="RightSlot">⌘ R</div>
+            </Menubar.Item>
+            <Menubar.Item className="MenubarItem inset" disabled>
+              Force Reload <div className="RightSlot">⇧ ⌘ R</div>
+            </Menubar.Item>
+            <Menubar.Separator className="MenubarSeparator" />
+            <Menubar.Item className="MenubarItem inset">Toggle Fullscreen</Menubar.Item>
+            <Menubar.Separator className="MenubarSeparator" />
+            <Menubar.Item className="MenubarItem inset">Hide Sidebar</Menubar.Item>
+          </Menubar.Content>
+        </Menubar.Portal>
+      </Menubar.Menu>
+
+      <Menubar.Menu>
+        <Menubar.Trigger className="MenubarTrigger">Profiles</Menubar.Trigger>
+        <Menubar.Portal>
+          <Menubar.Content
+            className="MenubarContent"
+            align="start"
+            sideOffset={5}
+            alignOffset={-14}
+          >
+            <Menubar.RadioGroup value={radioSelection} onValueChange={setRadioSelection}>
+              {RADIO_ITEMS.map((item) => (
+                <Menubar.RadioItem className="MenubarRadioItem inset" key={item} value={item}>
+                  <Menubar.ItemIndicator className="MenubarItemIndicator">
+                    <DotFilledIcon />
+                  </Menubar.ItemIndicator>
+                  {item}
+                </Menubar.RadioItem>
+              ))}
+              <Menubar.Separator className="MenubarSeparator" />
+              <Menubar.Item className="MenubarItem inset">Edit…</Menubar.Item>
+              <Menubar.Separator className="MenubarSeparator" />
+              <Menubar.Item className="MenubarItem inset">Add Profile…</Menubar.Item>
+            </Menubar.RadioGroup>
+          </Menubar.Content>
+        </Menubar.Portal>
+      </Menubar.Menu>
+    </Menubar.Root>
+  );
+};
+
+export default MenubarDemo;
---
+++ components/demos/Menubar/stitches/index.jsx
@@ -0,0 +1,261 @@
+import React from 'react';
+import * as Menubar from '@radix-ui/react-menubar';
+import { styled } from '@modulz/design-system';
+import { blackA, violet, mauve } from '@radix-ui/colors';
+import { CheckIcon, ChevronRightIcon, DotFilledIcon } from '@radix-ui/react-icons';
+
+const RADIO_ITEMS = ['Andy', 'Benoît', 'Luis'];
+const CHECK_ITEMS = ['Always Show Bookmarks Bar', 'Always Show Full URLs'];
+
+const MenubarDemo = () => {
+  const [checkedSelection, setCheckedSelection] = React.useState([CHECK_ITEMS[1]]);
+  const [radioSelection, setRadioSelection] = React.useState(RADIO_ITEMS[2]);
+
+  return (
+    <MenubarRoot>
+      <Menubar.Menu>
+        <MenubarTrigger>File</MenubarTrigger>
+        <Menubar.Portal>
+          <MenubarContent align="start" sideOffset={5} alignOffset={-3}>
+            <MenubarItem>
+              New Tab <RightSlot>⌘ T</RightSlot>
+            </MenubarItem>
+            <MenubarItem>
+              New Window <RightSlot>⌘ N</RightSlot>
+            </MenubarItem>
+            <MenubarItem disabled> New Incognito Window</MenubarItem>
+            <MenubarSeparator />
+            <Menubar.Sub>
+              <MenubarSubTrigger>
+                Share
+                <RightSlot>
+                  <ChevronRightIcon />
+                </RightSlot>
+              </MenubarSubTrigger>
+              <Menubar.Portal>
+                <MenubarSubContent alignOffset={-5}>
+                  <MenubarItem>Email Link</MenubarItem>
+                  <MenubarItem>Messages</MenubarItem>
+                  <MenubarItem>Notes</MenubarItem>
+                </MenubarSubContent>
+              </Menubar.Portal>
+            </Menubar.Sub>
+            <MenubarSeparator />
+            <MenubarItem>
+              Print… <RightSlot>⌘ P</RightSlot>
+            </MenubarItem>
+          </MenubarContent>
+        </Menubar.Portal>
+      </Menubar.Menu>
+
+      <Menubar.Menu>
+        <MenubarTrigger>Edit</MenubarTrigger>
+        <Menubar.Portal>
+          <MenubarContent align="start" sideOffset={5} alignOffset={-3}>
+            <MenubarItem>
+              Undo <RightSlot>⌘ Z</RightSlot>
+            </MenubarItem>
+            <MenubarItem>
+              Redo <RightSlot>⇧ ⌘ Z</RightSlot>
+            </MenubarItem>
+            <MenubarSeparator />
+            <Menubar.Sub>
+              <MenubarSubTrigger>
+                Find
+                <RightSlot>
+                  <ChevronRightIcon />
+                </RightSlot>
+              </MenubarSubTrigger>
+
+              <Menubar.Portal>
+                <MenubarSubContent alignOffset={-5}>
+                  <MenubarItem>Search the web…</MenubarItem>
+                  <MenubarSeparator />
+                  <MenubarItem>Find…</MenubarItem>
+                  <MenubarItem>Find Next</MenubarItem>
+                  <MenubarItem>Find Previous</MenubarItem>
+                </MenubarSubContent>
+              </Menubar.Portal>
+            </Menubar.Sub>
+            <MenubarSeparator />
+            <MenubarItem>Cut</MenubarItem>
+            <MenubarItem>Copy</MenubarItem>
+            <MenubarItem>Paste</MenubarItem>
+          </MenubarContent>
+        </Menubar.Portal>
+      </Menubar.Menu>
+
+      <Menubar.Menu>
+        <MenubarTrigger>View</MenubarTrigger>
+        <Menubar.Portal>
+          <MenubarContent align="start" sideOffset={5} alignOffset={-14}>
+            {CHECK_ITEMS.map((item) => (
+              <MenubarCheckboxItem
+                key={item}
+                checked={checkedSelection.includes(item)}
+                variant="inset"
+                onCheckedChange={() =>
+                  setCheckedSelection((current) =>
+                    current.includes(item)
+                      ? current.filter((el) => el !== item)
+                      : current.concat(item)
+                  )
+                }
+              >
+                <MenubarItemIndicator>
+                  <CheckIcon />
+                </MenubarItemIndicator>
+                {item}
+              </MenubarCheckboxItem>
+            ))}
+            <MenubarSeparator />
+            <MenubarItem variant="inset">
+              Reload <RightSlot>⌘ R</RightSlot>
+            </MenubarItem>
+            <MenubarItem variant="inset" disabled>
+              Force Reload <RightSlot>⇧ ⌘ R</RightSlot>
+            </MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem variant="inset">Toggle Fullscreen</MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem variant="inset">Hide Sidebar</MenubarItem>
+          </MenubarContent>
+        </Menubar.Portal>
+      </Menubar.Menu>
+
+      <Menubar.Menu>
+        <MenubarTrigger>Profiles</MenubarTrigger>
+        <Menubar.Portal>
+          <MenubarContent align="start" sideOffset={5} alignOffset={-14}>
+            <Menubar.RadioGroup value={radioSelection} onValueChange={setRadioSelection}>
+              {RADIO_ITEMS.map((item) => (
+                <MenubarRadioItem key={item} value={item} variant="inset">
+                  <MenubarItemIndicator>
+                    <DotFilledIcon />
+                  </MenubarItemIndicator>
+                  {item}
+                </MenubarRadioItem>
+              ))}
+              <MenubarSeparator />
+              <MenubarItem variant="inset">Edit…</MenubarItem>
+              <MenubarSeparator />
+              <MenubarItem variant="inset">Add Profile…</MenubarItem>
+            </Menubar.RadioGroup>
+          </MenubarContent>
+        </Menubar.Portal>
+      </Menubar.Menu>
+    </MenubarRoot>
+  );
+};
+
+const MenubarRoot = styled(Menubar.Root, {
+  display: 'flex',
+  backgroundColor: 'white',
+  padding: 3,
+  borderRadius: 6,
+  boxShadow: `0 2px 10px ${blackA.blackA7}`,
+});
+
+const MenubarTrigger = styled(Menubar.Trigger, {
+  all: 'unset',
+  padding: '8px 12px',
+  outline: 'none',
+  userSelect: 'none',
+  fontWeight: 500,
+  lineHeight: 1,
+  borderRadius: 4,
+  fontSize: 13,
+  color: violet.violet11,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  gap: 2,
+
+  '&[data-highlighted], &[data-state="open"]': {
+    backgroundColor: violet.violet4,
+  },
+});
+
+const contentStyles = {
+  minWidth: 220,
+  backgroundColor: 'white',
+  borderRadius: 6,
+  padding: 5,
+  boxShadow:
+    '0px 10px 38px -10px rgba(22, 23, 24, 0.35), 0px 10px 20px -15px rgba(22, 23, 24, 0.2)',
+  animationDuration: '400ms',
+  animationTimingFunction: 'cubic-bezier(0.16, 1, 0.3, 1)',
+  willChange: 'transform, opacity',
+};
+
+const MenubarContent = styled(Menubar.Content, contentStyles);
+const MenubarSubContent = styled(Menubar.SubContent, contentStyles);
+
+const itemStyles = {
+  all: 'unset',
+  fontSize: 13,
+  lineHeight: 1,
+  color: violet.violet11,
+  borderRadius: 4,
+  display: 'flex',
+  alignItems: 'center',
+  height: 25,
+  padding: '0 10px',
+  position: 'relative',
+  userSelect: 'none',
+
+  '&[data-disabled]': {
+    color: mauve.mauve8,
+    pointerEvents: 'none',
+  },
+
+  '&[data-highlighted]': {
+    backgroundImage: `linear-gradient(135deg, ${violet.violet9} 0%, ${violet.violet10} 100%)`,
+    color: violet.violet1,
+  },
+
+  variants: {
+    variant: {
+      inset: {
+        paddingLeft: 20,
+      },
+    },
+  },
+};
+
+const MenubarItem = styled(Menubar.Item, itemStyles);
+const MenubarSubTrigger = styled(Menubar.SubTrigger, {
+  '&[data-state="open"]': {
+    backgroundColor: violet.violet4,
+    color: violet.violet11,
+  },
+  ...itemStyles,
+});
+
+const MenubarCheckboxItem = styled(Menubar.CheckboxItem, itemStyles);
+const MenubarRadioItem = styled(Menubar.RadioItem, itemStyles);
+
+const MenubarItemIndicator = styled(Menubar.ItemIndicator, {
+  position: 'absolute',
+  left: 0,
+  width: 20,
+  display: 'inline-flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+});
+
+const RightSlot = styled('div', {
+  marginLeft: 'auto',
+  paddingLeft: 20,
+  color: mauve.mauve9,
+  '[data-highlighted] > &': { color: 'white' },
+  '[data-disabled] &': { color: mauve.mauve8 },
+});
+
+const MenubarSeparator = styled(Menubar.Separator, {
+  height: 1,
+  backgroundColor: violet.violet6,
+  margin: 5,
+});
+
+export default MenubarDemo;
