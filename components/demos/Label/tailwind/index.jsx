31a90f9b60f229f9d6a46fe33c6060a553d8cafb
shadcn <m@shadcn.com>
2023-02-09 22:01:00 +0400

    feat(label): add tailwind demo

---
+++ components/demos/Label/tailwind/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import * as Label from '@radix-ui/react-label';
+
+const LabelDemo = () => (
+  <div className="flex flex-wrap items-center gap-[15px] px-5">
+    <Label.Root className="text-[15px] font-medium leading-[35px] text-white" htmlFor="firstName">
+      First name
+    </Label.Root>
+    <input
+      className="bg-blackA5 shadow-blackA9 inline-flex h-[35px] w-[200px] appearance-none items-center justify-center rounded-[4px] px-[10px] text-[15px] leading-none text-white shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px_black]"
+      type="text"
+      id="firstName"
+      defaultValue="Pedro Duarte"
+    />
+  </div>
+);
+
+export default LabelDemo;
