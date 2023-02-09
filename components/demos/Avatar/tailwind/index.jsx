75b3a7736a88f74352abebdc0a54ad0a5b8cb782
shadcn <m@shadcn.com>
2023-02-09 21:43:44 +0400

    feat(avatar): add tailwind demo

---
+++ components/demos/Avatar/tailwind/index.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import * as Avatar from '@radix-ui/react-avatar';
+
+const AvatarDemo = () => (
+  <div className="flex gap-5">
+    <Avatar.Root className="bg-blackA3 inline-flex h-[45px] w-[45px] select-none items-center justify-center overflow-hidden rounded-full align-middle">
+      <Avatar.Image
+        className="h-full w-full rounded-[inherit] object-cover"
+        src="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
+        alt="Colm Tuite"
+      />
+      <Avatar.Fallback
+        className="text-violet11 leading-1 flex h-full w-full items-center justify-center bg-white text-[15px] font-medium"
+        delayMs={600}
+      >
+        CT
+      </Avatar.Fallback>
+    </Avatar.Root>
+    <Avatar.Root className="bg-blackA3 inline-flex h-[45px] w-[45px] select-none items-center justify-center overflow-hidden rounded-full align-middle">
+      <Avatar.Image
+        className="h-full w-full rounded-[inherit] object-cover"
+        src="https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&w=128&h=128&dpr=2&q=80"
+        alt="Pedro Duarte"
+      />
+      <Avatar.Fallback
+        className="text-violet11 leading-1 flex h-full w-full items-center justify-center bg-white text-[15px] font-medium"
+        delayMs={600}
+      >
+        JD
+      </Avatar.Fallback>
+    </Avatar.Root>
+    <Avatar.Root className="bg-blackA3 inline-flex h-[45px] w-[45px] select-none items-center justify-center overflow-hidden rounded-full align-middle">
+      <Avatar.Fallback className="text-violet11 leading-1 flex h-full w-full items-center justify-center bg-white text-[15px] font-medium">
+        PD
+      </Avatar.Fallback>
+    </Avatar.Root>
+  </div>
+);
+
+export default AvatarDemo;
