1d86c9f85179af7ba0c6eaa9f4edd068c36b9270
shadcn <m@shadcn.com>
2023-02-10 00:00:40 +0400

    feat(label): add tailwind demo (#581)

---
+++ components/demos/Label/tailwind/tailwind.config.js
@@ -0,0 +1,14 @@
+const { blackA } = require('@radix-ui/colors');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./App.jsx'],
+  theme: {
+    extend: {
+      colors: {
+        ...blackA,
+      },
+    },
+  },
+  plugins: [],
+};
