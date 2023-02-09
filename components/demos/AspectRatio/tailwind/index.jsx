8c60713a1d1c972e5412fd3126be0b138a47cfe2
shadcn <m@shadcn.com>
2023-02-09 21:31:27 +0400

    feat(aspect-ratio): add tailwind demo

---
+++ components/demos/AspectRatio/tailwind/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import * as AspectRatio from '@radix-ui/react-aspect-ratio';
+
+const AspectRatioDemo = () => (
+  <div className="shadow-blackA7 w-[300px] overflow-hidden rounded-md shadow-[0_2px_10px]">
+    <AspectRatio.Root ratio={16 / 9}>
+      <img
+        className="h-full w-full object-cover"
+        src="https://images.unsplash.com/photo-1535025183041-0991a977e25b?w=300&dpr=2&q=80"
+        alt="Landscape photograph by Tobias Tullius"
+      />
+    </AspectRatio.Root>
+  </div>
+);
+
+export default AspectRatioDemo;
