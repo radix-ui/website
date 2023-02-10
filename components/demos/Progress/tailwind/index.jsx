e38d0f116279f04e80e614072337c064173564ae
João Pedro Magalhães <joaopsilvamagalhaes@gmail.com>
2023-02-09 22:46:17 -0300

    Add Tailwind CSS support to Progress

---
+++ components/demos/Progress/tailwind/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import * as Progress from '@radix-ui/react-progress';
+
+const ProgressDemo = () => {
+  const [progress, setProgress] = React.useState(13);
+
+  React.useEffect(() => {
+    const timer = setTimeout(() => setProgress(66), 500);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <Progress.Root
+      className="relative overflow-hidden bg-blackA9 rounded-full w-[300px] h-[25px]"
+      style={{
+        // Fix overflow clipping in Safari
+        // https://gist.github.com/domske/b66047671c780a238b51c51ffde8d3a0
+        transform: 'translateZ(0)',
+      }}
+      value={66}
+    >
+      <Progress.Indicator
+        className="bg-white w-full h-full transition-transform duration-[660ms] ease-[cubic-bezier(0.65, 0, 0.35, 1)]"
+        style={{ transform: `translateX(-${100 - progress}%)` }}
+      />
+    </Progress.Root>
+  );
+};
+
+export default ProgressDemo;
