857cb66461b6ff406fd30622ff07a3444970e28e
Kukuh Sulistyo <sulistyo.kukuh@gmail.com>
2023-02-10 17:22:34 +0700

    [Tailwind] Add tailwind demo for Toggle (#590)
    * docs: add tailwind demo for Toggle
    
    * fix: use arbitrary values
    
    ---------
    
    Co-authored-by: Kukuh Sulistyo <kukuh.aji@wartek.belajar.id>

---
+++ components/demos/Toggle/tailwind/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import * as Toggle from '@radix-ui/react-toggle';
+import { FontItalicIcon } from '@radix-ui/react-icons';
+
+const ToggleDemo = () => (
+  <Toggle.Root
+    aria-label="Toggle italic"
+    className="hover:bg-violet3 color-mauve11 data-[state=on]:bg-violet6 data-[state=on]:text-violet12 shadow-blackA7 flex h-[35px] w-[35px] items-center justify-center rounded bg-white text-base leading-4 shadow-[0_2px_10px] focus:shadow-[0_0_0_2px] focus:shadow-black"
+  >
+    <FontItalicIcon />
+  </Toggle.Root>
+);
+
+export default ToggleDemo;
