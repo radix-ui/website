ee944a7b067b247dd121151c0da443087493155c
Ryan Leichty <ryan.leichty@gmail.com>
2023-02-09 19:29:59 -0800

    Add Tailwind CSS support to Select

---
+++ components/demos/Select/tailwind/tailwind.config.js
@@ -0,0 +1,15 @@
+const { mauve, violet } = require('@radix-ui/colors');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./App.jsx'],
+  theme: {
+    extend: {
+      colors: {
+        ...mauve,
+        ...violet,
+      },
+    },
+  },
+  plugins: [],
+};
