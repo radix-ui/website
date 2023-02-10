b24429f22d6c6341bf4df0766c0ec4a0ccc01a64
João Pedro Magalhães <joaopsilvamagalhaes@gmail.com>
2023-02-10 06:15:41 -0300

    Add Tailwind CSS support to Tabs (#589)

---
+++ components/demos/Tabs/tailwind/tailwind.config.js
@@ -0,0 +1,17 @@
+const { blackA, green, mauve, violet } = require('@radix-ui/colors');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./App.jsx'],
+  theme: {
+    extend: {
+      colors: {
+        ...blackA,
+        ...green,
+        ...mauve,
+        ...violet,
+      },
+    },
+  },
+  plugins: [],
+};
