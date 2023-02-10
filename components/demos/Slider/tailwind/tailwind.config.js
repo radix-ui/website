013cca980426eb61f0b792db79e4c4d80ee8c3f3
João Pedro Magalhães <joaopsilvamagalhaes@gmail.com>
2023-02-10 06:12:18 -0300

    Add Tailwind CSS support to Slider (#587)

---
+++ components/demos/Slider/tailwind/tailwind.config.js
@@ -0,0 +1,15 @@
+const { blackA, violet } = require('@radix-ui/colors');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./App.jsx'],
+  theme: {
+    extend: {
+      colors: {
+        ...blackA,
+        ...violet,
+      },
+    },
+  },
+  plugins: [],
+};
