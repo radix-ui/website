2105f2fb38715f33a9aa46b740ced5b81dd5abf6
Benoît Grélard <benoit.grelard@gmail.com>
2023-02-12 19:46:02 +0000

    Update components/demos/ToggleGroup/tailwind/index.jsx

--- components/demos/ToggleGroup/tailwind/index.jsx
+++ components/demos/ToggleGroup/tailwind/index.jsx
@@ -3,7 +3,7 @@ import * as ToggleGroup from '@radix-ui/react-toggle-group';
 import { TextAlignLeftIcon, TextAlignCenterIcon, TextAlignRightIcon } from '@radix-ui/react-icons';
 
 const toggleGroupItemClasses =
-  'hover:bg-violet3 color-mauve11 data-[state=on]:bg-violet6 data-[state=on]:text-violet12 shadow-blackA7 flex h-[35px] w-[35px] items-center justify-center bg-white text-base leading-4 shadow-[0_2px_10px] first:rounded-l last:rounded-r focus:z-10 focus:shadow-[0_0_0_2px] focus:shadow-black';
+  'hover:bg-violet3 color-mauve11 data-[state=on]:bg-violet6 data-[state=on]:text-violet12 flex h-[35px] w-[35px] items-center justify-center bg-white text-base leading-4 first:rounded-l last:rounded-r focus:z-10 focus:shadow-[0_0_0_2px] focus:shadow-black focus:outline-none';
 
 const ToggleGroupDemo = () => (
   <ToggleGroup.Root
