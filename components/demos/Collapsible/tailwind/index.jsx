c6bd93299f09dd525ccbe864246a95f7866048bf
Ryan Leichty <ryan.leichty@gmail.com>
2023-02-12 14:54:01 -0500

    Add Tailwind CSS support to Collapsible (#595)

---
+++ components/demos/Collapsible/tailwind/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import * as Collapsible from '@radix-ui/react-collapsible';
+import { RowSpacingIcon, Cross2Icon } from '@radix-ui/react-icons';
+
+const CollapsibleDemo = () => {
+  const [open, setOpen] = React.useState(false);
+  return (
+    <Collapsible.Root className="w-[300px]" open={open} onOpenChange={setOpen}>
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+        <span className="text-violet11 text-[15px] leading-[25px]" style={{ color: 'white' }}>
+          @peduarte starred 3 repositories
+        </span>
+        <Collapsible.Trigger asChild>
+          <button className="rounded-full h-[25px] w-[25px] inline-flex items-center justify-center text-violet11 shadow-[0_2px_10px] shadow-blackA7 outline-none data-[state=closed]:bg-white data-[state=open]:bg-violet3 hover:bg-violet3 focus:shadow-[0_0_0_2px] focus:shadow-black">
+            {open ? <Cross2Icon /> : <RowSpacingIcon />}
+          </button>
+        </Collapsible.Trigger>
+      </div>
+
+      <div className="bg-white rounded my-[10px] p-[10px] shadow-[0_2px_10px] shadow-blackA7">
+        <span className="text-violet11 text-[15px] leading-[25px]">@radix-ui/primitives</span>
+      </div>
+
+      <Collapsible.Content>
+        <div className="bg-white rounded my-[10px] p-[10px] shadow-[0_2px_10px] shadow-blackA7">
+          <span className="text-violet11 text-[15px] leading-[25px]">@radix-ui/colors</span>
+        </div>
+        <div className="bg-white rounded my-[10px] p-[10px] shadow-[0_2px_10px] shadow-blackA7">
+          <span className="text-violet11 text-[15px] leading-[25px]">@stitches/react</span>
+        </div>
+      </Collapsible.Content>
+    </Collapsible.Root>
+  );
+};
+
+export default CollapsibleDemo;
