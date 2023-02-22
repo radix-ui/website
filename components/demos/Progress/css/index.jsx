56dbd5b18f952e5a2f6e1ac8414e7e82fbdc4146
Stefan Mai <stefan.mai@iamnafets.com>
2023-02-22 14:09:09 -0800

    Fix progress example

--- components/demos/Progress/css/index.jsx
+++ components/demos/Progress/css/index.jsx
@@ -11,7 +11,7 @@ const ProgressDemo = () => {
   }, []);
 
   return (
-    <Progress.Root className="ProgressRoot" value={66}>
+    <Progress.Root className="ProgressRoot" value={progress}>
       <Progress.Indicator
         className="ProgressIndicator"
         style={{ transform: `translateX(-${100 - progress}%)` }}
