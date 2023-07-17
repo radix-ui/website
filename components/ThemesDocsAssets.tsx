48832503879555db67a824b1b61402bfb326bf8d
Andy Hook <hello@andyhook.dev>
2023-07-17 13:15:08 +0100

    Tidy tokens assets, improve copy

--- components/ThemesDocsAssets.tsx
+++ components/ThemesDocsAssets.tsx
@@ -1,26 +1,14 @@
-import {
-  AccessibleIcon,
-  Box,
-  Code,
-  Flex,
-  Grid,
-  IconButton,
-  Popover,
-  Separator,
-  Text,
-} from '@radix-ui/themes';
+import { AccessibleIcon, Box, Code, Flex, Grid, IconButton, Popover, Text } from '@radix-ui/themes';
 import { RegionTable } from './RegionTable';
 import { InfoCircledIcon } from '@radix-ui/react-icons';
 
 export function ThemesBorderRadiusTable() {
   return (
-    <Flex align="center" gap="4" mt="6" mb="4">
+    <Flex gap="4" mt="6" mb="5">
       {[...new Array(6)].map((_, i) => (
         <Flex direction="column" key={i} grow="1" align="center" gap="3">
           <Box height="8" width="100%">
-            <DecorativeBox
-              style={{ borderStyle: 'solid', borderRadius: `var(--radius-${i + 1})` }}
-            />
+            <DecorativeBox style={{ borderRadius: `var(--radius-${i + 1})` }} />
           </Box>
 
           <Text size="1" color="gray">
@@ -34,7 +22,7 @@ export function ThemesBorderRadiusTable() {
 
 export function ThemesShadowTable() {
   return (
-    <Flex direction="column" gap="1" mt="5" mb="3">
+    <Flex direction="column" gap="3" mt="6" mb="5">
       <Flex
         style={{ flex: 1, backgroundColor: 'var(--gray-4)', borderRadius: 'var(--radius-3)' }}
         p="5"
@@ -58,130 +46,51 @@ export function ThemesShadowTable() {
 
       <Flex align="center" gap="1" px="4">
         {[...new Array(5)].map((_, i) => (
-          <Box style={{ flex: 1 }} height="6" key={i}>
-            <Flex align="center" justify="center" height="100%" width="100%">
-              <Text size="1" color="gray">
-                {i + 1}
-              </Text>
-            </Flex>
-          </Box>
+          <Flex align="center" justify="center" height="100%" width="100%" key={i}>
+            <Text size="1" color="gray">
+              {i + 1}
+            </Text>
+          </Flex>
         ))}
       </Flex>
     </Flex>
   );
 }
 
-export function ThemesSwatchTable({ type = 'accent' }: { type: 'accent' | 'gray' }) {
+export function ThemesColorTable({ type = 'accent' }: { type: 'accent' | 'gray' }) {
   return (
-    <Flex direction="column" gap="1" mt="6" mb="5">
+    <Flex direction="column" gap="3" mt="6" mb="5">
       <Flex align="center" gap="1">
         {[...new Array(12)].map((_, i) => (
           <Flex grow="1" direction="column" height="9" gap="1" key={i}>
-            <Box style={{ flex: 1 }}>
-              <Box
-                style={{
-                  backgroundColor: `var(--${type}-${i + 1})`,
-                  borderRadius: 'var(--radius-2)',
-                }}
-                height="100%"
-                width="100%"
-              />
-            </Box>
-            <Box style={{ flex: 1 }}>
-              <Box
-                style={{
-                  backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none'%3E%3Crect width='12' height='12' fill='%23002F75' fill-opacity='0.02'/%3E%3Crect x='12' y='12' width='12' height='12' fill='%23002F75' fill-opacity='0.02'/%3E%3C/svg%3E")`,
-                  backgroundColor: `var(--${type}-a${i + 1})`,
-                  borderRadius: 'var(--radius-2)',
-                }}
-                height="100%"
-                width="100%"
-              />
-            </Box>
+            <Box
+              grow="1"
+              style={{
+                backgroundColor: `var(--${type}-${i + 1})`,
+                borderRadius: 'var(--radius-2)',
+              }}
+            />
+
+            <Box
+              grow="1"
+              style={{
+                backgroundImage: `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none'%3E%3Crect width='12' height='12' fill='%23002F75' fill-opacity='0.02'/%3E%3Crect x='12' y='12' width='12' height='12' fill='%23002F75' fill-opacity='0.02'/%3E%3C/svg%3E")`,
+                backgroundColor: `var(--${type}-a${i + 1})`,
+                borderRadius: 'var(--radius-2)',
+              }}
+            />
           </Flex>
         ))}
       </Flex>
 
       <Flex align="center" gap="1">
         {[...new Array(12)].map((_, i) => (
-          <Box style={{ flex: 1 }} height="6" key={i}>
-            <Flex align="center" justify="center" height="100%" width="100%">
-              <Text size="1" color="gray">
-                {i + 1}
-              </Text>
-            </Flex>
-          </Box>
+          <Flex align="center" justify="center" height="100%" width="100%" key={i}>
+            <Text size="1" color="gray">
+              {i + 1}
+            </Text>
+          </Flex>
         ))}
-      </Flex>
-    </Flex>
-  );
-}
-
-export function ThemesThemeAnatomy() {
-  return (
-    <Flex
-      style={{
-        backgroundColor: 'var(--gray-a6)',
-        borderRadius: 'var(--radius-4)',
-        position: 'relative',
-      }}
-      align="center"
-      justify="center"
-      my="7"
-      p="7"
-    >
-      <Flex
-        align="center"
-        justify="end"
-        style={{
-          backgroundColor: 'var(--gray-11)',
-          width: 200,
-          height: 2,
-          position: 'absolute',
-          top: '30%',
-          left: 100,
-        }}
-      >
-        <Box
-          style={{ borderRadius: '100%', backgroundColor: 'var(--gray-11)' }}
-          width="2"
-          height="2"
-        />
-      </Flex>
-
-      <Flex
-        align="center"
-        justify="start"
-        style={{
-          backgroundColor: 'var(--gray-11)',
-          width: 200,
-          height: 2,
-          position: 'absolute',
-          bottom: '30%',
-          right: 100,
-        }}
-      >
-        <Box
-          style={{ borderRadius: '100%', backgroundColor: 'var(--gray-11)' }}
-          width="2"
-          height="2"
-        />
-      </Flex>
-
-      <Flex
-        align="center"
-        justify="center"
-        style={{
-          backgroundColor: 'var(--color-panel)',
-          borderRadius: 'var(--radius-4)',
-          width: '100%',
-          maxWidth: 300,
-          height: 300,
-          boxShadow: 'var(--shadow-3)',
-        }}
-        p="5"
-      >
-        <Text color="gray">Annotated theme UI</Text>
       </Flex>
     </Flex>
   );
@@ -189,7 +98,7 @@ export function ThemesThemeAnatomy() {
 
 export function ThemesSpacingTable() {
   return (
-    <Flex align="end" gap="1" mt="8" mb="5">
+    <Flex align="end" gap="1" mt="7" mb="5">
       {[...new Array(9)].map((_, i) => (
         <Flex direction="column" grow="1" key={i} align="center" gap="4">
           <DecorativeBox style={{ width: '100%', height: `var(--space-${i + 1})` }} />
@@ -205,7 +114,7 @@ export function ThemesSpacingTable() {
 
 export function ThemesFontSizeTable() {
   return (
-    <Box pb="6" mt="5" mb="8">
+    <Box pb="6" mt="6" mb="5">
       <Flex align="stretch" style={{ borderBottom: '1px dashed var(--gray-a6)' }}>
         {[...new Array(9)].map((_, i) => (
           <Flex
@@ -220,7 +129,7 @@ export function ThemesFontSizeTable() {
               Aa
             </Text>
 
-            <Flex position="absolute" style={{ bottom: 'calc(var(--space-6) * -1)' }}>
+            <Flex position="absolute" style={{ top: 'calc(100% + var(--space-4))' }}>
               <Text size="1" color="gray">
                 {i + 1}
               </Text>
@@ -232,108 +141,10 @@ export function ThemesFontSizeTable() {
   );
 }
 
-export function ThemesFontFamilyTable({
-  data,
-  'aria-label': ariaLabel,
-  'aria-labelledby': ariaLabelledBy,
-}: {
-  data: {
-    token: React.ReactNode;
-    value: React.ReactNode;
-  }[];
-  'aria-label'?: string;
-  'aria-labelledby'?: string;
-}) {
-  const hasAriaLabel = !!(ariaLabel || ariaLabelledBy);
-  return (
-    <RegionTable
-      style={{ width: '100%', textAlign: 'left', borderCollapse: 'collapse' }}
-      aria-label={hasAriaLabel ? ariaLabel : 'Keyboard Interactions'}
-      aria-labelledby={ariaLabelledBy}
-    >
-      <thead>
-        <tr>
-          <Box
-            asChild
-            py="3"
-            pr="4"
-            style={{
-              width: '30%',
-              borderBottom: '1px solid var(--gray-a6)',
-            }}
-          >
-            <th>
-              <Text as="p" size="2" color="gray">
-                Token
-              </Text>
-            </th>
-          </Box>
-          <Box
-            asChild
-            py="3"
-            pr="4"
-            style={{
-              borderBottom: '1px solid var(--gray-a6)',
-            }}
-          >
-            <th>
-              <Text as="p" size="2" color="gray">
-                Base value
-              </Text>
-            </th>
-          </Box>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map(({ token, value }, i) => (
-          <tr key={i}>
-            <Box
-              asChild
-              pr="4"
-              py="3"
-              style={{
-                borderBottom: '1px solid var(--gray-a6)',
-                whiteSpace: 'nowrap',
-                width: '30%',
-              }}
-            >
-              <td>
-                <Code
-                  color="gray"
-                  size="2"
-                  style={{
-                    whiteSpace: 'nowrap',
-                  }}
-                >
-                  {token}
-                </Code>
-              </td>
-            </Box>
-
-            <Box
-              asChild
-              py="3"
-              style={{
-                borderBottom: '1px solid var(--gray-a6)',
-              }}
-            >
-              <td>
-                <Code size="2" color="gray">
-                  {value}
-                </Code>
-              </td>
-            </Box>
-          </tr>
-        ))}
-      </tbody>
-    </RegionTable>
-  );
-}
-
-export function ThemesAdditionalSwatches() {
+export function ThemesAdditionalColors() {
   return (
     <Flex direction="column" gap="5" mt="6" mb="7">
-      <Grid columns={{ initial: '5', sm: '7' }} gap="2">
+      <Grid columns={{ initial: '3', xs: '4', sm: '7' }} gap="2">
         {([
           'tomato',
           'red',
@@ -378,7 +189,7 @@ export function ThemesAdditionalSwatches() {
         ))}
       </Grid>
 
-      <Grid columns={{ initial: '5', sm: '7' }} gap="3">
+      <Grid columns={{ initial: '3', xs: '4', sm: '7' }} gap="3">
         {['mauve', 'slate', 'sage', 'olive', 'sand'].map((color, i) => (
           <Box grow="1" key={i}>
             <Flex
@@ -558,7 +369,7 @@ export function ThemesFontSizingTable({
   );
 }
 
-export function ThemesTokensTable({
+export function ThemesTokensDescriptionTable({
   data,
   'aria-label': ariaLabel,
   'aria-labelledby': ariaLabelledBy,
@@ -657,7 +468,7 @@ export function ThemesTokensTable({
   );
 }
 
-export function ThemesTokensTableValues({
+export function ThemesTokenValuesTable({
   data,
   'aria-label': ariaLabel,
   'aria-labelledby': ariaLabelledBy,
@@ -789,6 +600,76 @@ export function ThemesTokensTableValues({
   );
 }
 
+export function ThemesAnatomy() {
+  return (
+    <Flex
+      style={{
+        backgroundColor: 'var(--gray-a6)',
+        borderRadius: 'var(--radius-4)',
+        position: 'relative',
+      }}
+      align="center"
+      justify="center"
+      my="7"
+      p="7"
+    >
+      <Flex
+        align="center"
+        justify="end"
+        style={{
+          backgroundColor: 'var(--gray-11)',
+          width: 200,
+          height: 2,
+          position: 'absolute',
+          top: '30%',
+          left: 100,
+        }}
+      >
+        <Box
+          style={{ borderRadius: '100%', backgroundColor: 'var(--gray-11)' }}
+          width="2"
+          height="2"
+        />
+      </Flex>
+
+      <Flex
+        align="center"
+        justify="start"
+        style={{
+          backgroundColor: 'var(--gray-11)',
+          width: 200,
+          height: 2,
+          position: 'absolute',
+          bottom: '30%',
+          right: 100,
+        }}
+      >
+        <Box
+          style={{ borderRadius: '100%', backgroundColor: 'var(--gray-11)' }}
+          width="2"
+          height="2"
+        />
+      </Flex>
+
+      <Flex
+        align="center"
+        justify="center"
+        style={{
+          backgroundColor: 'var(--color-panel)',
+          borderRadius: 'var(--radius-4)',
+          width: '100%',
+          maxWidth: 300,
+          height: 300,
+          boxShadow: 'var(--shadow-3)',
+        }}
+        p="5"
+      >
+        <Text color="gray">Annotated theme UI</Text>
+      </Flex>
+    </Flex>
+  );
+}
+
 export function DecorativeBox(props) {
   return (
     <Box
@@ -796,7 +677,7 @@ export function DecorativeBox(props) {
       style={{
         height: '100%',
         backgroundColor: 'var(--gray-a3)',
-        border: '1px dashed var(--gray-a7)',
+        border: '1px solid var(--gray-a7)',
         borderRadius: 'var(--radius-1)',
         backgroundImage: `url("data:image/svg+xml,%3Csvg width='6' height='6' viewBox='0 0 6 6' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='%239C92AC' fill-opacity='0.2' fill-rule='evenodd'%3E%3Cpath d='M5 0h1L0 6V5zM6 5v1H5z'/%3E%3C/g%3E%3C/svg%3E")`,
         ...props.style,
