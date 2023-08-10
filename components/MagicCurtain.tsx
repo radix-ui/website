5dfb448fae7070425e450327c7c5958968dda6ad
Vlad Moroz <hi@vladmoroz.com>
2023-08-10 17:04:56 +0200

    A good start

--- components/MagicCurtain.tsx
+++ components/MagicCurtain.tsx
@@ -1,69 +1,61 @@
 import * as React from 'react';
 import styles from './MagicCurtain.module.css';
 import { createContext } from '@radix-ui/react-context';
+import * as NavigationMenu from '@radix-ui/react-navigation-menu';
 
-type Visibility = 'hidden' | 'visible-in-front' | 'visible-behind';
+type Visibility = 'hidden' | 'animating-out' | 'visible';
+type ForceReducedMotion = 'always' | 'if-hi-res' | 'never';
 
 interface MagicCurtainItem {
-  ref: React.RefObject<HTMLElement>;
+  visibility: Visibility;
   setVisibility: (visibility: Visibility) => void;
-  setAnimationDirection: (direction: string) => void;
-  setAnimationPlayState: (playState: string) => void;
+  ref: React.RefObject<HTMLDivElement>;
 }
 
 const [MagicCurtainProvider, useMagicCurtainContext] = createContext<{
-  itemsRef: React.RefObject<MagicCurtainItem[]>;
+  items: MagicCurtainItem[];
+  setItems: React.Dispatch<React.SetStateAction<MagicCurtainItem[]>>;
+
+  // Which control is highlighted as active
+  highlightedControl: string;
+  setHighlightedControl: React.Dispatch<React.SetStateAction<string>>;
+
+  // Which control is hovered (hovering the preview also counts)
+  hoveredControl: string;
+  setHoveredControl: React.Dispatch<React.SetStateAction<string>>;
 }>('MagicCurtain');
 
 const MagicCurtainRoot = ({ children }: React.PropsWithChildren<{}>) => {
-  const itemsRef = React.useRef<MagicCurtainItem[]>([]);
+  const [items, setItems] = React.useState<MagicCurtainItem[]>([]);
+  const [hoveredControl, setHoveredControl] = React.useState<string>('');
+  const [highlightedControl, setHighlightedControl] = React.useState<string>('0');
   const ref = React.useRef<HTMLDivElement>(null);
+  const [forceReducedMotion, setForceReducedMotion] = React.useState<ForceReducedMotion>('never');
 
-  useIsomorphicLayoutEffect(() => {
-    const handleAnimationEnd = (event: AnimationEvent) => {
-      if (
-        !(event.target instanceof HTMLElement) ||
-        ![styles['magic-curtain'], styles['magic-curtain-reverse']].includes(event.animationName)
-      ) {
-        return;
-      }
-
-      const thisIndex = itemsRef.current.map((item) => item.ref.current).indexOf(event.target);
-      const nextIndex = thisIndex + 1 === itemsRef.current.length ? 0 : thisIndex + 1;
-      const afterNextIndex = nextIndex + 1 === itemsRef.current.length ? 0 : nextIndex + 1;
-      itemsRef.current[thisIndex]?.setVisibility('hidden');
-      itemsRef.current[nextIndex]?.setVisibility('visible-in-front');
-      itemsRef.current[afterNextIndex]?.setVisibility('visible-behind');
-    };
-
-    itemsRef.current[0]?.setVisibility('visible-in-front');
-
-    if (/Android|iPhone|Firefox/i.test(navigator.userAgent)) {
-      return;
+  React.useEffect(() => {
+    if (/firefox/i.test(navigator.userAgent)) {
+      setForceReducedMotion('if-hi-res');
     }
 
-    itemsRef.current[1]?.setVisibility('visible-behind');
-
-    itemsRef.current.forEach((item, i) => {
-      item.setAnimationPlayState('running');
-
-      if (i % 2) {
-        item.setAnimationDirection('reverse');
-      }
-
-      item.ref.current?.addEventListener('animationend', handleAnimationEnd);
-    });
-
-    return () => {
-      itemsRef.current.forEach((item) => {
-        item.ref.current?.removeEventListener('animationend', handleAnimationEnd);
-      });
-    };
+    if (/iphone|android/i.test(navigator.userAgent)) {
+      setForceReducedMotion('always');
+    }
   }, []);
 
   return (
-    <MagicCurtainProvider itemsRef={itemsRef}>
-      <div ref={ref} className={styles.MagicCurtainRoot}>
+    <MagicCurtainProvider
+      items={items}
+      setItems={setItems}
+      highlightedControl={highlightedControl}
+      setHighlightedControl={setHighlightedControl}
+      hoveredControl={hoveredControl}
+      setHoveredControl={setHoveredControl}
+    >
+      <div
+        ref={ref}
+        data-force-reduced-motion={forceReducedMotion}
+        className={styles.MagicCurtainRoot}
+      >
         {children}
       </div>
     </MagicCurtainProvider>
@@ -76,103 +68,283 @@ const MagicCurtainItem = ({
   ...props
 }: React.ComponentPropsWithoutRef<'div'> & {
   defaultVisibility?: Visibility;
-  'data-animation-play-state'?: 'running' | 'paused';
-  'data-animation-direction'?: 'normal' | 'reverse';
 }) => {
   const context = useMagicCurtainContext('MagicCurtain');
   const ref = React.useRef<HTMLDivElement>(null);
   const [visibility, setVisibility] = React.useState<Visibility>(defaultVisibility);
-  const [animationDirection, setAnimationDirection] = React.useState('normal');
-  const [animationPlayState, setAnimationPlayState] = React.useState('paused');
-  const delayRef = React.useRef<ReturnType<typeof setTimeout>>();
-
-  React.useEffect(() => {
-    const handlePointerDown = () => {
-      clearTimeout(delayRef.current);
-      setAnimationPlayState('paused');
-      addEventListener('pointerup', handlePointerUp, { once: true });
-
-      // Release the pause state in case the pointer moves out of the document boundaries without pointerup event
-      clearTimeout(delayRef.current);
-      delayRef.current = setTimeout(() => {
-        setAnimationPlayState('running');
-      }, 3000);
-    };
-
-    const handlePointerUp = () => {
-      clearTimeout(delayRef.current);
-      delayRef.current = setTimeout(() => {
-        setAnimationPlayState('running');
-      }, 1500);
-    };
-
-    const handleScroll = () => {
-      clearTimeout(delayRef.current);
-      delayRef.current = setTimeout(() => {
-        setAnimationPlayState('running');
-      }, 500);
-    };
-
-    const handleKeyDown = () => {
-      clearTimeout(delayRef.current);
-      setAnimationPlayState('paused');
-
-      delayRef.current = setTimeout(() => {
-        setAnimationPlayState('running');
-      }, 1500);
-    };
-
-    const handleAnimationStart = (event: Event) => {
-      if (event.target === ref.current) {
-        clearListeners();
-      }
-    };
-
-    const clearListeners = () => {
-      removeEventListener('pointerdown', handlePointerDown);
-      removeEventListener('pointerup', handlePointerUp);
-      removeEventListener('touchstart', handlePointerUp);
-      removeEventListener('keydown', handleKeyDown);
-      removeEventListener('scroll', handleScroll, { capture: true });
-      ref.current?.removeEventListener('animationstart', handleAnimationStart);
-      clearTimeout(delayRef.current);
-    };
-
-    if (visibility === 'visible-in-front') {
-      addEventListener('pointerdown', handlePointerDown);
-      addEventListener('keydown', handleKeyDown);
-      addEventListener('scroll', handleScroll, { capture: true, passive: true });
-      ref.current?.addEventListener('animationstart', handleAnimationStart, { once: true });
-    } else {
-      clearListeners();
-    }
-
-    return () => clearListeners();
-  }, [visibility]);
 
   useIsomorphicLayoutEffect(() => {
-    const item = { ref, setVisibility, setAnimationDirection, setAnimationPlayState };
-    context.itemsRef.current.push(item);
+    const item = { ref, visibility, setVisibility };
+
+    context.setItems((items) =>
+      [...items, item].sort((a, b) => {
+        // Sort items according to their order in the DOM
+        return a.ref.current.compareDocumentPosition(b.ref.current) & 4 ? -1 : 1;
+      })
+    );
 
     return () => {
-      const index = context.itemsRef.current.indexOf(item);
-
-      if (index > -1) {
-        context.itemsRef.current.splice(index, 1);
-      }
+      context.setItems((items) => {
+        return [...items.filter((value) => value.ref !== item.ref)];
+      });
     };
-  }, []);
+  }, [visibility]);
 
   return (
-    <div
-      data-animation-play-state={animationPlayState}
-      data-animation-direction={animationDirection}
-      data-visibility={visibility}
-      ref={ref}
-      className={styles.MagicCurtainItem}
-      {...props}
-    >
+    <div data-visibility={visibility} ref={ref} className={styles.MagicCurtainItem} {...props}>
       {visibility === 'hidden' ? null : children}
+    </div>
+  );
+};
+
+interface MagicCurtainControlsProps {
+  images?: string[];
+}
+
+const MagicCurtainControls = ({ images }: MagicCurtainControlsProps) => {
+  const context = useMagicCurtainContext('MagicCurtain');
+  const items = context.items;
+  const viewportWrapperRef = React.useRef<HTMLDivElement>(null);
+  const [menuValue, setMenuValue] = React.useState<string>('');
+  const [offsetIndex, setOffsetIndex] = React.useState<string>('');
+  const hasAnimatingItem = !!items.find((value) => value.visibility === 'animating-out');
+  const upcomingAnimationCallback = React.useRef<(() => void) | null>(null);
+
+  // Clear offset on viewport removal
+  React.useLayoutEffect(() => {
+    const observer = new MutationObserver((mutationList) => {
+      for (const mutation of mutationList) {
+        if (mutation.type === 'childList') {
+          const firstRemovedNode = mutation.removedNodes[0] as HTMLElement | undefined;
+
+          if (firstRemovedNode?.getAttribute('data-viewport')) {
+            setOffsetIndex('');
+          }
+        }
+      }
+    });
+
+    observer.observe(viewportWrapperRef.current, { childList: true });
+    return () => observer.disconnect();
+  }, []);
+
+  const startAnimation = React.useCallback(
+    (clickedItem: MagicCurtainItem) => {
+      // Do nothing if this item is already visible
+      if (clickedItem.visibility === 'visible') {
+        return;
+      }
+
+      const itemToHide = items.find((value) => value.visibility === 'visible');
+
+      const handleAnimationEnd = (event: AnimationEvent) => {
+        // Make sure this is the right animation
+        const isMagicCurtainAnimation = [
+          styles['magic-curtain-fade'],
+          styles['magic-curtain-clip'],
+        ].includes(event.animationName);
+
+        if (event.target instanceof HTMLElement && isMagicCurtainAnimation) {
+          itemToHide.setVisibility('hidden');
+          event.currentTarget.removeEventListener('animationend', handleAnimationEnd);
+
+          // Run the rescheduled animation callback if it’s there
+          upcomingAnimationCallback.current?.();
+        }
+      };
+
+      clickedItem.setVisibility('visible');
+      itemToHide?.setVisibility('animating-out');
+      itemToHide?.ref.current?.addEventListener('animationend', handleAnimationEnd);
+    },
+    [context]
+  );
+
+  return (
+    <NavigationMenu.Root
+      delayDuration={40}
+      skipDelayDuration={40}
+      value={menuValue}
+      className={styles.MagicCurtainControlsRoot}
+      onValueChange={(value) => {
+        setMenuValue(value);
+        setOffsetIndex(value);
+      }}
+    >
+      <NavigationMenu.List
+        style={{
+          display: 'flex',
+          listStyle: 'none',
+          margin: 0,
+          padding: 0,
+          zIndex: 1,
+          position: 'relative',
+        }}
+      >
+        {items.map((item, index) => (
+          <NavigationMenu.Item
+            key={index}
+            className={styles.MagicCurtainControlsItem}
+            value={index.toString()}
+            onPointerMove={() => context.setHoveredControl(index.toString())}
+            onPointerLeave={() => {
+              // Wait a tick and reset the hovered control index if it wasn't changed by anything else
+              setTimeout(() => {
+                context.setHoveredControl((current) =>
+                  current === index.toString() ? '' : current
+                );
+              });
+            }}
+          >
+            <NavigationMenu.Trigger
+              data-visually-hidden
+              aria-label={`Example ${index}`}
+              data-visibility={item.visibility}
+              className={styles.MagicCurtainControlsTrigger}
+              onClick={() => {
+                // Do nothing if this item is already visible
+                if (item.visibility === 'visible') {
+                  return;
+                }
+
+                // Always set the clicked control as highlighted,
+                // even if it's going to be animated a bit later
+                context.setHighlightedControl(index.toString());
+
+                if (hasAnimatingItem) {
+                  // Schedule the animation start after the current animation ends
+                  upcomingAnimationCallback.current = () => {
+                    startAnimation(item);
+                    upcomingAnimationCallback.current = null;
+                  };
+                } else {
+                  startAnimation(item);
+                }
+              }}
+              onKeyDown={(event) => {
+                if (event.key !== 'ArrowRight' && event.key !== 'ArrowLeft') {
+                  return;
+                }
+
+                let nextItem = event.key === 'ArrowLeft' ? items[index - 1] : items[index + 1];
+
+                // This is for when we are focused on the item that isn’t currently active and press an arrow key.
+                // We want to activate the item on either arrow key press first before we can continue cycling through
+                // the list. We check for `!hasAnimatingItem` so that if there is an animation running and we are
+                // rapidly cycling through the list with the arrow keys, it doesn't stutter over the hidden items.
+                if (item.visibility === 'hidden' && !hasAnimatingItem) {
+                  // Don’t move the focus to the next/previous item
+                  event.preventDefault();
+                  nextItem = item;
+                }
+
+                // No looping
+                if (items.indexOf(nextItem) === -1) {
+                  return;
+                }
+
+                // Always set the clicked control as highlighted,
+                // even if it's going to be animated a bit later
+                context.setHighlightedControl(items.indexOf(nextItem).toString());
+
+                if (hasAnimatingItem) {
+                  upcomingAnimationCallback.current = () => {
+                    // Schedule the animation start after the current animation ends
+                    startAnimation(nextItem);
+                    upcomingAnimationCallback.current = null;
+                  };
+                } else {
+                  startAnimation(nextItem);
+                }
+              }}
+            />
+
+            <div style={{ display: 'none' }}>
+              {images[index] && (
+                <NavigationMenu.Content
+                  forceMount
+                  data-active={menuValue === index.toString()}
+                  className={styles.MagicCurtainControlsPreviewContent}
+                  onPointerMove={() => context.setHoveredControl(index.toString())}
+                  onPointerLeave={() => {
+                    // Wait a tick and reset the hovered control index if it wasn't changed by anything else
+                    setTimeout(() => {
+                      context.setHoveredControl((current) =>
+                        current === index.toString() ? '' : current
+                      );
+                    });
+                  }}
+                  onClick={() => {
+                    // Force hide the hover card and dispel the hover state
+                    setMenuValue('');
+                    context.setHoveredControl('');
+
+                    // Do nothing if this item is already visible
+                    if (item.visibility === 'visible') {
+                      return;
+                    }
+
+                    // Always set the clicked control as highlighted,
+                    // even if it's going to be animated a bit later
+                    context.setHighlightedControl(index.toString());
+
+                    if (hasAnimatingItem) {
+                      // Schedule the animation start after the current animation ends
+                      upcomingAnimationCallback.current = () => {
+                        startAnimation(item);
+                        upcomingAnimationCallback.current = null;
+                      };
+                    } else {
+                      startAnimation(item);
+                    }
+                  }}
+                >
+                  <img
+                    className={styles.MagicCurtainControlsPreviewContentImage}
+                    src={images[index]}
+                  />
+                </NavigationMenu.Content>
+              )}
+            </div>
+          </NavigationMenu.Item>
+        ))}
+      </NavigationMenu.List>
+
+      <div
+        ref={viewportWrapperRef}
+        className={styles.MagicCurtainControlsPreviewViewportWrapper}
+        style={
+          {
+            // Avoid transitioning from initial position when first opening
+            display: offsetIndex ? undefined : 'none',
+            '--magic-curtain-controls-offset-index': offsetIndex,
+          } as React.CSSProperties
+        }
+      >
+        <NavigationMenu.Viewport
+          data-viewport
+          className={styles.MagicCurtainControlsPreviewViewport}
+        />
+      </div>
+    </NavigationMenu.Root>
+  );
+};
+
+const MagicCurtainMirrorControls = () => {
+  const context = useMagicCurtainContext('MagicCurtain');
+  return (
+    <div className={styles.MagicCurtainControlsRoot}>
+      {context.items.map((item, index) => (
+        <div key={index} className={styles.MagicCurtainControlsItem}>
+          <div
+            data-highlighted={
+              context.hoveredControl === index.toString() ||
+              context.highlightedControl === index.toString()
+            }
+            className={styles.MagicCurtainControlsTrigger}
+          />
+        </div>
+      ))}
     </div>
   );
 };
@@ -183,4 +355,6 @@ const useIsomorphicLayoutEffect =
 export const MagicCurtain = {
   Root: MagicCurtainRoot,
   Item: MagicCurtainItem,
+  Controls: MagicCurtainControls,
+  MirrorControls: MagicCurtainMirrorControls,
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -31,19 +31,12 @@ export default function ThemesHome() {
 
       <Head>
         <meta name="theme-color" content="#FDFCFD" />
-        <style>
-          {`
-            .rt-Card.rt-Card.rt-variant-surface::after {
-              box-shadow: var(--shadow-4);
-            }
-          `}
-        </style>
       </Head>
 
       <ThemesMobileMenu />
 
       <MagicCurtain.Root>
-        <MagicCurtain.Item defaultVisibility="visible-in-front">
+        <MagicCurtain.Item defaultVisibility="visible">
           <Theme hasBackground accentColor="indigo" grayColor="slate">
             <Box height="0">
               <ThemesHeader ghost />
@@ -63,6 +56,10 @@ export default function ThemesHome() {
                   <ExampleThemesDashboard align="start" />
                 </Theme>
               </ThemesHeroLayout.Showcase>
+
+              <ControlsWrapper>
+                <MagicCurtain.MirrorControls />
+              </ControlsWrapper>
             </ThemesHeroLayout.Root>
           </Theme>
         </MagicCurtain.Item>
@@ -87,6 +84,10 @@ export default function ThemesHome() {
                   <ExampleThemesEcommerce align="start" />
                 </Theme>
               </ThemesHeroLayout.Showcase>
+
+              <ControlsWrapper>
+                <MagicCurtain.MirrorControls />
+              </ControlsWrapper>
             </ThemesHeroLayout.Root>
           </Theme>
         </MagicCurtain.Item>
@@ -111,6 +112,10 @@ export default function ThemesHome() {
                   <ExampleThemesMusicApp align="start" />
                 </Theme>
               </ThemesHeroLayout.Showcase>
+
+              <ControlsWrapper>
+                <MagicCurtain.MirrorControls />
+              </ControlsWrapper>
             </ThemesHeroLayout.Root>
           </Theme>
         </MagicCurtain.Item>
@@ -135,6 +140,10 @@ export default function ThemesHome() {
                   <ExampleThemesDashboard align="start" />
                 </Theme>
               </ThemesHeroLayout.Showcase>
+
+              <ControlsWrapper>
+                <MagicCurtain.MirrorControls />
+              </ControlsWrapper>
             </ThemesHeroLayout.Root>
           </Theme>
         </MagicCurtain.Item>
@@ -164,6 +173,10 @@ export default function ThemesHome() {
                   <ExampleThemesEcommerce align="start" />
                 </Theme>
               </ThemesHeroLayout.Showcase>
+
+              <ControlsWrapper>
+                <MagicCurtain.MirrorControls />
+              </ControlsWrapper>
             </ThemesHeroLayout.Root>
           </Theme>
         </MagicCurtain.Item>
@@ -188,13 +201,43 @@ export default function ThemesHome() {
                   <ExampleThemesMusicApp align="start" />
                 </Theme>
               </ThemesHeroLayout.Showcase>
+
+              <ControlsWrapper>
+                <MagicCurtain.MirrorControls />
+              </ControlsWrapper>
             </ThemesHeroLayout.Root>
           </Theme>
         </MagicCurtain.Item>
+
+        <Theme appearance="light" hasBackground={false} asChild>
+          <ControlsWrapper style={{ zIndex: 100, color: 'transparent' }}>
+            <MagicCurtain.Controls
+              images={[
+                'https://ph-files.imgix.net/e3c8c9b2-f0cf-41b0-9d68-1fc683afa6dd.png?auto=compress&codec=mozjpeg&cs=strip&auto=format&w=387&h=220&fit=max&dpr=2',
+                'https://ph-files.imgix.net/1818db79-e35e-4b86-93af-b3fc0904326f.png?auto=compress&codec=mozjpeg&cs=strip&auto=format&w=387&h=220&fit=max&dpr=2',
+                'https://ph-files.imgix.net/5e1a6ef4-adee-472b-9bd1-782454bc2287.png?auto=compress&codec=mozjpeg&cs=strip&auto=format&w=387&h=220&fit=max&dpr=2',
+                'https://ph-files.imgix.net/1818db79-e35e-4b86-93af-b3fc0904326f.png?auto=compress&codec=mozjpeg&cs=strip&auto=format&w=387&h=220&fit=max&dpr=2',
+                'https://ph-files.imgix.net/e3c8c9b2-f0cf-41b0-9d68-1fc683afa6dd.png?auto=compress&codec=mozjpeg&cs=strip&auto=format&w=387&h=220&fit=max&dpr=2',
+                'https://ph-files.imgix.net/1818db79-e35e-4b86-93af-b3fc0904326f.png?auto=compress&codec=mozjpeg&cs=strip&auto=format&w=387&h=220&fit=max&dpr=2',
+              ]}
+            />
+          </ControlsWrapper>
+        </Theme>
       </MagicCurtain.Root>
     </MobileMenuProvider>
   );
 }
+
+const ControlsWrapper = ({ children, ...props }: React.ComponentPropsWithoutRef<typeof Box>) => {
+  return (
+    <Box position="fixed" bottom="0" left="0" ml="5" mb="5" {...props}>
+      <Text weight="bold" size="2" style={{ pointerEvents: 'none' }}>
+        Explore examples
+      </Text>
+      {children}
+    </Box>
+  );
+};
 
 const MainContent = ({
   codeBlockScheme = 'indigo',
@@ -226,23 +269,25 @@ const MainContent = ({
       <Text size={{ initial: '4', xs: '5' }}>
         <Text as="p" mb="5" style={{ maxWidth: 500 }} color="gray">
           An open source component library optimized for fast development, easy maintenance,
-          and accessibility.
+          and accessibility. Just import and go—no configuration required.
         </Text>
 
-        <Box mb="3">
+        <Box mb="5">
           <SyntaxSchemeProvider scheme={codeBlockScheme}>
             <CodeBlock language="jsx" value={codeExample} style={codeStyles} />
           </SyntaxSchemeProvider>
         </Box>
       </Text>
 
-      <Text as="p" size="3" mb="6" color="gray">
-        Just import and go—no configuration required.
-      </Text>
-
-      <Flex gap="4" direction={{ initial: 'column', xs: 'row' }}>
+      <Flex mb="6" gap="4" direction={{ initial: 'column', xs: 'row' }}>
         <NextLink href="/themes/docs/overview/getting-started" passHref>
-          <Button asChild size={{ initial: '3', xs: '4' }} color="gray" highContrast>
+          <Button
+            asChild
+            size={{ initial: '3', xs: '4' }}
+            color="gray"
+            highContrast
+            style={{ flexGrow: 1 }}
+          >
             <a>
               Get started
               <svg
@@ -259,10 +304,16 @@ const MainContent = ({
           </Button>
         </NextLink>
         <NextLink href="/themes/playground" passHref>
-          <Button asChild size={{ initial: '3', xs: '4' }} variant="soft" highContrast>
+          <Button
+            asChild
+            size={{ initial: '3', xs: '4' }}
+            variant="soft"
+            highContrast
+            style={{ flexGrow: 1 }}
+          >
             <a>
               <GridIcon width="18" height="18" style={{ marginTop: 1 }} />
-              Explore components
+              Playground
             </a>
           </Button>
         </NextLink>
@@ -348,12 +399,6 @@ const BackgroundImage = ({
         d="M1597.13 169.784L2575.97 540.796C2632.38 562.177 2672.57 600.783 2685.46 645.97L2846.44 1210.15L3577.41 984.049C3635.96 965.94 3702.63 967.837 3759.03 989.218L4737.87 1360.23C4841.74 1399.6 4881.91 1492.55 4827.61 1567.85L4567 1929.16C4544.3 1960.63 4507.28 1985.44 4462.22 1999.38L3184.4 2394.63L3465.53 3379.94C3475.61 3415.29 3468.23 3452.09 3444.66 3483.9L3078.69 3977.79L1920.2 3538.68C1863.79 3517.3 1823.6 3478.7 1810.71 3433.51L1649.74 2869.33L918.759 3095.43C860.213 3113.54 793.545 3111.64 737.138 3090.26L-421.356 2651.15L-70.6875 2151.46C-48.1049 2119.28 -10.63 2093.89 35.1782 2079.72L1311.78 1684.85L1030.38 698.593C1020.45 663.815 1027.43 627.619 1050.13 596.147L1310.73 234.835C1365.04 159.539 1493.27 130.415 1597.13 169.784Z"
         fill={`url(#paint2_radial_37_453-${id})`}
       />
-      <g filter={`url(#filter0_f_37_453-${id})`}>
-        <path
-          d="M2395.71 -658.308L3374.55 -287.296C3430.96 -265.915 3471.15 -227.309 3484.04 -182.122L3645.01 382.06L4375.99 155.958C4434.54 137.848 4501.2 139.745 4557.61 161.126L5536.45 532.138C5640.32 571.507 5680.49 664.461 5626.18 739.757L5365.58 1101.07C5342.88 1132.54 5305.86 1157.35 5260.8 1171.29L3982.97 1566.54L4264.1 2551.84C4274.19 2587.2 4266.81 2624 4243.24 2655.81L3877.27 3149.7L2718.78 2710.59C2662.37 2689.21 2622.18 2650.6 2609.29 2605.42L2448.31 2041.24L1717.34 2267.34C1658.79 2285.45 1592.12 2283.55 1535.72 2262.17L377.222 1823.06L727.891 1323.37C750.473 1291.19 787.948 1265.8 833.756 1251.63L2110.35 856.754L1828.95 -129.498C1819.03 -164.277 1826.01 -200.472 1848.71 -231.944L2109.31 -593.257C2163.62 -668.552 2291.85 -697.677 2395.71 -658.308Z"
-          fill={`url(#paint3_radial_37_453-${id})`}
-        />
-      </g>
       <path
         d="M3059.26 767.932L3310.25 1618.16C3324.72 1667.15 3315.74 1727.88 3285.79 1783.6L2911.89 2479.3L3514.51 2558.36C3562.77 2564.69 3599.15 2596.78 3613.62 2645.77L3864.61 3496C3891.25 3586.22 3837.41 3706.98 3744.37 3765.74L3297.91 4047.66C3259.03 4072.22 3217.48 4082.97 3180.34 4078.1L2126.89 3939.89L1473.9 5154.88C1450.47 5198.48 1415.9 5235.81 1376.24 5260.35L760.412 5641.34L463.348 4635.06C448.884 4586.06 457.863 4525.33 487.81 4469.61L861.713 3773.92L259.094 3694.86C210.828 3688.53 174.448 3656.44 159.984 3607.44L-137.08 2601.17L474.823 2206.89C514.228 2181.5 556.514 2170.3 594.278 2175.25L1646.71 2313.32L2300.33 1097.17C2323.38 1054.28 2357.22 1017.43 2396.11 992.876L2842.57 710.953C2935.61 652.202 3032.62 677.712 3059.26 767.932Z"
         fill={`url(#paint4_radial_37_453-${id})`}
