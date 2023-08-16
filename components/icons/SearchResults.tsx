d9a74d8525e51777fc252ba5c8cb2d8828e51054
Andy Hook <hello@andyhook.dev>
2023-08-16 15:40:39 +0100

    Replace portal usage to fix hydration error

--- components/icons/SearchResults.tsx
+++ components/icons/SearchResults.tsx
@@ -1,7 +1,7 @@
 import * as Icons from '@radix-ui/react-icons';
 import { Button, Flex, Grid, Text } from '@radix-ui/themes';
 import React from 'react';
-import { CopyToastVisibility } from './CopyToast';
+import { useCopyToast } from './CopyToast';
 
 type SearchResultsProps = {
   value: string;
@@ -30,84 +30,80 @@ const iconNames = Object.keys(Icons).map((key) => {
 });
 
 export const SearchResults = ({ value }: SearchResultsProps) => {
+  const { showCopyToast } = useCopyToast();
   const cleanValue = escapeStringRegexp(value.trim().replace(/\s/g, ' '));
   const matchingNames = iconNames.filter((name) => new RegExp(`\\b${cleanValue}`, 'gi').test(name));
 
   return (
-    <CopyToastVisibility.Consumer>
-      {({ setIcon, setIsVisible }) => (
-        <>
-          {value && matchingNames.length > 0 && (
-            <Grid
-              flow={{ initial: 'row', sm: 'column' }}
-              gapX="6"
-              gapY="5"
-              rows={{
-                sm: `repeat(${Math.max(Math.ceil(matchingNames.length / 2), 3)}, auto)`,
-                md: `repeat(${Math.max(Math.ceil(matchingNames.length / 4), 3)}, auto)`,
-              }}
-              columns={{ initial: '1', sm: 'repeat(2, 1fr)', md: 'repeat(4, 1fr)' }}
-              py={{ initial: '3', sm: '6' }}
-              px={{ initial: '4', sm: '7' }}
-            >
-              {matchingNames.map((name) => (
-                <Button
-                  key={name}
-                  style={{ justifyContent: 'flex-start' }}
-                  variant="ghost"
-                  highContrast
-                  size="3"
-                  onClick={(event: React.MouseEvent) => {
-                    const svg = event.currentTarget.querySelector('svg');
-                    const code = svg && svg.parentElement ? svg.parentElement.innerHTML : null;
+    <>
+      {value && matchingNames.length > 0 && (
+        <Grid
+          flow={{ initial: 'row', sm: 'column' }}
+          gapX="6"
+          gapY="5"
+          rows={{
+            sm: `repeat(${Math.max(Math.ceil(matchingNames.length / 2), 3)}, auto)`,
+            md: `repeat(${Math.max(Math.ceil(matchingNames.length / 4), 3)}, auto)`,
+          }}
+          columns={{ initial: '1', sm: 'repeat(2, 1fr)', md: 'repeat(4, 1fr)' }}
+          py={{ initial: '3', sm: '6' }}
+          px={{ initial: '4', sm: '7' }}
+        >
+          {matchingNames.map((name) => (
+            <Button
+              key={name}
+              style={{ justifyContent: 'flex-start' }}
+              variant="ghost"
+              highContrast
+              size="3"
+              onClick={(event: React.MouseEvent) => {
+                const svg = event.currentTarget.querySelector('svg');
+                const code = svg && svg.parentElement ? svg.parentElement.innerHTML : null;
 
-                    // Copy code to clipboard via a hidden textarea element
-                    if (code) {
-                      // Temporary shim until a proper focus-visible handler is added
-                      if (document.activeElement instanceof HTMLButtonElement) {
-                        document.activeElement.blur();
-                      }
+                // Copy code to clipboard via a hidden textarea element
+                if (code) {
+                  // Temporary shim until a proper focus-visible handler is added
+                  if (document.activeElement instanceof HTMLButtonElement) {
+                    document.activeElement.blur();
+                  }
 
-                      const textarea = document.createElement('textarea');
-                      textarea.value = code.toString();
-                      textarea.setAttribute('readonly', '');
-                      textarea.style.position = 'absolute';
-                      textarea.style.left = '-9999px';
-                      document.body.appendChild(textarea);
-                      textarea.select();
-                      document.execCommand('copy');
-                      document.body.removeChild(textarea);
+                  const textarea = document.createElement('textarea');
+                  textarea.value = code.toString();
+                  textarea.setAttribute('readonly', '');
+                  textarea.style.position = 'absolute';
+                  textarea.style.left = '-9999px';
+                  document.body.appendChild(textarea);
+                  textarea.select();
+                  document.execCommand('copy');
+                  document.body.removeChild(textarea);
 
-                      // Show CopyToast and set latest icon
-                      setIsVisible();
-                      setIcon(code);
-                    }
-                  }}
-                >
-                  {React.createElement(Object.values(Icons)[iconNames.indexOf(name)] as any)}
-                  {name}
-                </Button>
-              ))}
-            </Grid>
-          )}
-          {!matchingNames.length && (
-            <Flex
-              align="center"
-              justify="center"
-              py="6"
-              px="6"
-              style={{
-                minHeight: 300,
+                  // Show CopyToast and set latest icon
+                  showCopyToast(code);
+                }
               }}
             >
-              <Text size="2" style={{ textAlign: 'center' }}>
-                No icons for <span style={{ fontWeight: 500 }}>{value}</span>
-              </Text>
-            </Flex>
-          )}
-        </>
+              {React.createElement(Object.values(Icons)[iconNames.indexOf(name)] as any)}
+              {name}
+            </Button>
+          ))}
+        </Grid>
       )}
-    </CopyToastVisibility.Consumer>
+      {!matchingNames.length && (
+        <Flex
+          align="center"
+          justify="center"
+          py="6"
+          px="6"
+          style={{
+            minHeight: 300,
+          }}
+        >
+          <Text size="2" style={{ textAlign: 'center' }}>
+            No icons for <span style={{ fontWeight: 500 }}>{value}</span>
+          </Text>
+        </Flex>
+      )}
+    </>
   );
 };
 
