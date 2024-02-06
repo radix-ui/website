5b175346f8e69f576b7b618c99caabd0e0c9293f
Benoît Grélard <benoit.grelard@gmail.com>
2024-02-06 20:50:58 +0000

    Fix scroll jumps due to RemoveScroll

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,6 +9,7 @@ import { useRouter } from 'next/router';
 import { useMobileMenuContext } from './MobileMenu';
 import { classNames } from '@lib/classNames';
 import { RadixLogo, RadixLogoIcon } from './RadixLogo';
+import { RemoveScroll } from 'react-remove-scroll';
 
 export interface HeaderProps {
   children?: React.ReactNode;
@@ -52,115 +53,127 @@ export const Header = ({ children, gitHubLink, ghost }: HeaderProps) => {
         className={classNames(styles.HeaderRoot, ghost ? styles.ghost : '')}
       >
         <div className={styles.HeaderInner}>
-          <Flex
-            display={{ xs: 'none' }}
-            align="center"
-            position="absolute"
-            top="0"
-            bottom="0"
-            left="0"
-            pl="4"
+          {/* Components that hide the scrollbar (like Dialog) add padding to
+          account for the scrollbar gap to avoid layout jank. This does not
+          work for position: fixed elements. Since we use react-remove-scroll
+          under the hood for those primitives, we can add this helper class
+          provided by that lib to deal with that for the Header.
+          https://github.com/radix-ui/website/issues/64
+          https://github.com/theKashey/react-remove-scroll#positionfixed-elements */}
+          <div
+            className={RemoveScroll.classNames.fullWidth}
+            style={{ position: 'absolute', height: 'inherit', top: 0, left: 0, right: 0 }}
           >
-            <NextLink href="/" passHref legacyBehavior>
-              <BoxLink>
-                <AccessibleIcon label="Radix Homepage">
-                  {mobileMenu.open ? <RadixLogoIcon /> : <RadixLogo />}
-                </AccessibleIcon>
-              </BoxLink>
-            </NextLink>
-          </Flex>
-
-          <Flex
-            display={{ initial: 'none', xs: 'flex' }}
-            align="center"
-            position="absolute"
-            top="0"
-            bottom="0"
-            left="0"
-            pl="4"
-          >
-            <NextLink href="/" passHref legacyBehavior>
-              <BoxLink>
-                <AccessibleIcon label="Radix Homepage">
-                  <RadixLogo />
-                </AccessibleIcon>
-              </BoxLink>
-            </NextLink>
-          </Flex>
-
-          <div className={styles.HeaderProductLinksContainer}>
-            <HeaderProductLink
-              href="/"
-              active={router.pathname === '/' || router.pathname.startsWith('/themes')}
+            <Flex
+              display={{ xs: 'none' }}
+              align="center"
+              position="absolute"
+              top="0"
+              bottom="0"
+              left="0"
+              pl="4"
             >
-              Themes
-            </HeaderProductLink>
-            <HeaderProductLink
-              href="/primitives"
-              active={router.pathname.startsWith('/primitives')}
+              <NextLink href="/" passHref legacyBehavior>
+                <BoxLink>
+                  <AccessibleIcon label="Radix Homepage">
+                    {mobileMenu.open ? <RadixLogoIcon /> : <RadixLogo />}
+                  </AccessibleIcon>
+                </BoxLink>
+              </NextLink>
+            </Flex>
+
+            <Flex
+              display={{ initial: 'none', xs: 'flex' }}
+              align="center"
+              position="absolute"
+              top="0"
+              bottom="0"
+              left="0"
+              pl="4"
             >
-              Primitives
-            </HeaderProductLink>
-            <HeaderProductLink href="/icons" active={router.pathname.startsWith('/icons')}>
-              Icons
-            </HeaderProductLink>
-            <HeaderProductLink href="/colors" active={router.pathname.startsWith('/colors')}>
-              Colors
-            </HeaderProductLink>
-          </div>
+              <NextLink href="/" passHref legacyBehavior>
+                <BoxLink>
+                  <AccessibleIcon label="Radix Homepage">
+                    <RadixLogo />
+                  </AccessibleIcon>
+                </BoxLink>
+              </NextLink>
+            </Flex>
 
-          <Flex
-            display={{ initial: 'none', md: 'flex' }}
-            align="center"
-            gap="5"
-            position="absolute"
-            top="0"
-            bottom="0"
-            right="0"
-            pr="4"
-          >
-            {children}
-
-            {gitHubLink && (
-              <Tooltip className="radix-themes-custom-fonts" content="View GitHub ">
-                <IconButton asChild size="3" variant="ghost" color="gray">
-                  <a href={gitHubLink} target="_blank">
-                    <GitHubLogoIcon width="16" height="16" />
-                  </a>
-                </IconButton>
-              </Tooltip>
-            )}
-
-            <ThemeToggle />
-          </Flex>
-
-          <Flex
-            display={{ md: 'none' }}
-            align="center"
-            gap="4"
-            position="absolute"
-            top="0"
-            bottom="0"
-            right="0"
-            pr="4"
-          >
-            <div className={styles.HeaderThemeToggleContainer}>
-              <ThemeToggle />
+            <div className={styles.HeaderProductLinksContainer}>
+              <HeaderProductLink
+                href="/"
+                active={router.pathname === '/' || router.pathname.startsWith('/themes')}
+              >
+                Themes
+              </HeaderProductLink>
+              <HeaderProductLink
+                href="/primitives"
+                active={router.pathname.startsWith('/primitives')}
+              >
+                Primitives
+              </HeaderProductLink>
+              <HeaderProductLink href="/icons" active={router.pathname.startsWith('/icons')}>
+                Icons
+              </HeaderProductLink>
+              <HeaderProductLink href="/colors" active={router.pathname.startsWith('/colors')}>
+                Colors
+              </HeaderProductLink>
             </div>
 
-            <Tooltip className="radix-themes-custom-fonts" content="Navigation">
-              <IconButton
-                size="3"
-                variant="ghost"
-                color="gray"
-                data-state={mobileMenu.open ? 'open' : 'closed'}
-                onClick={() => mobileMenu.setOpen((open) => !open)}
-                className={styles.MobileMenuButton}
-              >
-                <HamburgerMenuIcon width="16" height="16" />
-              </IconButton>
-            </Tooltip>
-          </Flex>
+            <Flex
+              display={{ initial: 'none', md: 'flex' }}
+              align="center"
+              gap="5"
+              position="absolute"
+              top="0"
+              bottom="0"
+              right="0"
+              pr="4"
+            >
+              {children}
+
+              {gitHubLink && (
+                <Tooltip className="radix-themes-custom-fonts" content="View GitHub ">
+                  <IconButton asChild size="3" variant="ghost" color="gray">
+                    <a href={gitHubLink} target="_blank">
+                      <GitHubLogoIcon width="16" height="16" />
+                    </a>
+                  </IconButton>
+                </Tooltip>
+              )}
+
+              <ThemeToggle />
+            </Flex>
+
+            <Flex
+              display={{ md: 'none' }}
+              align="center"
+              gap="4"
+              position="absolute"
+              top="0"
+              bottom="0"
+              right="0"
+              pr="4"
+            >
+              <div className={styles.HeaderThemeToggleContainer}>
+                <ThemeToggle />
+              </div>
+
+              <Tooltip className="radix-themes-custom-fonts" content="Navigation">
+                <IconButton
+                  size="3"
+                  variant="ghost"
+                  color="gray"
+                  data-state={mobileMenu.open ? 'open' : 'closed'}
+                  onClick={() => mobileMenu.setOpen((open) => !open)}
+                  className={styles.MobileMenuButton}
+                >
+                  <HamburgerMenuIcon width="16" height="16" />
+                </IconButton>
+              </Tooltip>
+            </Flex>
+          </div>
         </div>
       </div>
     </Theme>
