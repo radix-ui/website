2af5fb5697f1e866a9cc604e998f263d4cfc6ec1
Aarvin Roshin <aarvinr@outlook.com>
2024-08-20 20:24:18 -0700

    fix hero typo

--- pages/index.tsx
+++ pages/index.tsx
@@ -231,7 +231,7 @@ const MainContent = () => (
           <SerifHeading mb="3">
             Start building
             <br />
-            your appnow
+            your app now
           </SerifHeading>
         </Box>
         <Box display={{ initial: 'none', lg: 'block' }}>
