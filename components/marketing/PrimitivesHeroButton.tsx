b6bbb313bea08a7ab16c941133189f3eef6609b8
Chance Strickland <hi@chance.dev>
2024-08-22 10:01:22 -0700

    remove stitches: `PrimitivesHeroButton`

--- components/marketing/PrimitivesHeroButton.tsx
+++ components/marketing/PrimitivesHeroButton.tsx
@@ -1,61 +1,18 @@
-import { styled } from '@utils/stitches';
+import * as React from 'react';
+import styles from './PrimitivesHeroButton.module.css';
+import { classNames } from '@utils/classNames';
 
-export const PrimitivesHeroButton = styled('button', {
-  // Reset
-  all: 'unset',
-  alignItems: 'center',
-  boxSizing: 'border-box',
-  userSelect: 'none',
-  display: 'inline-flex',
-  color: 'var(--gray-12)',
-  fontSize: 'var(--font-size-1)',
-  lineHeight: 1,
-  gap: 3,
-  margin: 0,
-  paddingTop: 0,
-  paddingBottom: 0,
-  paddingLeft: 'var(--space-2)',
-  paddingRight: 'var(--space-2)',
-  height: 'var(--space-5)',
-  borderRadius: 'var(--radius-2)',
-
-  // For Safari layout shifts when interacting with positioned components
-  willChange: 'transform',
-
-  '&:focus-visible': {
-    boxShadow: '0 0 0 2px var(--gray-a8)',
-  },
-
-  variants: {
-    variant: {
-      white: {
-        backgroundColor: 'var(--white-a9)',
-        '@media (hover: hover)': {
-          '&:hover': {
-            backgroundColor: 'var(--white-a11)',
-          },
-        },
-        '.dark &, .dark-theme &': {
-          backgroundColor: 'var(--white-a3)',
-          '@media (hover: hover)': {
-            '&:hover': {
-              backgroundColor: 'var(--white-a4)',
-            },
-          },
-        },
-      },
-      gray: {
-        backgroundColor: 'var(--gray-a4)',
-        '@media (hover: hover)': {
-          '&:hover': {
-            backgroundColor: 'var(--gray-a5)',
-          },
-        },
-      },
-    },
-  },
-
-  defaultVariants: {
-    variant: 'white',
-  },
+export const PrimitivesHeroButton = React.forwardRef<
+  HTMLButtonElement,
+  React.ComponentProps<'button'> & { variant?: 'white' | 'gray' }
+>(function ({ variant = 'white', ...props }, ref) {
+  return (
+    <button
+      type="button"
+      data-variant={variant}
+      ref={ref}
+      {...props}
+      className={classNames(props.className, styles.PrimitivesHeroButton)}
+    />
+  );
 });
