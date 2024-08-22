0593b70d4c861240777a3c58f947bbad418655c7
Chance Strickland <hi@chance.dev>
2024-08-22 10:55:02 -0700

    remove stitches: `LogoLink`

--- components/marketing/LogoLink.tsx
+++ components/marketing/LogoLink.tsx
@@ -1,58 +1,11 @@
-import { styled } from '@utils/stitches';
+import * as React from 'react';
+import styles from './LogoLink.module.css';
 
-export const LogoLink = styled('a', {
-  display: 'block',
-  outline: 0,
-  color: 'var(--gray-12)',
-  position: 'relative',
-
-  '@media (hover: hover)': {
-    color: 'var(--gray-9)',
-    transition: '120ms color',
-
-    '&:hover': {
-      color: 'var(--gray-12)',
-    },
-  },
-
-  '&:focus-visible': {
-    zIndex: 1,
-    color: 'var(--gray-12)',
-    boxShadow: '0 0 0 2px var(--blue-8)',
-  },
-
-  variants: {
-    variant: {
-      normal: {
-        margin: 'calc(var(--space-1) * -1)',
-        padding: 'var(--space-1)',
-        borderRadius: 'var(--radius-2)',
-      },
-      box: {
-        boxShadow: '0 0 0 1px var(--gray-6)',
-        '&::before': {
-          content: '',
-          zIndex: 1,
-          position: 'absolute',
-          pointerEvents: 'none',
-          transition: '120ms opacity',
-          inset: 0,
-          opacity: 0,
-          boxShadow: '0 0 0 1px var(--gray-8)',
-        },
-        '&:hover::before': {
-          opacity: 1,
-        },
-        '&:focus::before': {
-          boxShadow: '0 0 0 1px var(--gray-8)',
-        },
-        '&:focus:not(:focus-visible)::before': {
-          opacity: 1,
-        },
-      },
-    },
-  },
-  defaultVariants: {
-    variant: 'normal',
-  },
+export const LogoLink = React.forwardRef<
+  HTMLAnchorElement,
+  React.ComponentProps<'a'> & {
+    variant?: 'normal' | 'box';
+  }
+>(function LogoLink({ variant = 'normal', ...props }, forwardedRef) {
+  return <a {...props} data-variant={variant} ref={forwardedRef} className={styles.LogoLink} />;
 });
