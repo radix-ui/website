ce2c9da33dffa3cbf8948ed648c2c11c9f20fdfc
Chance Strickland <hi@chance.dev>
2024-08-22 10:48:02 -0700

    remove stitches: `PrimitivesHeroToggleGroup`

--- components/marketing/PrimitivesHeroToggleGroup.tsx
+++ components/marketing/PrimitivesHeroToggleGroup.tsx
@@ -1,53 +1,11 @@
 import React from 'react';
-import { styled } from '@utils/stitches';
+import { styled } from '@utils/css';
 import { TextAlignCenterIcon, TextAlignLeftIcon, TextAlignRightIcon } from '@radix-ui/react-icons';
-
+import styles from './PrimitivesHeroToggleGroup.module.css';
 import * as ToggleGroupPrimitive from '@radix-ui/react-toggle-group';
 
-const StyledToggleGroup = styled(ToggleGroupPrimitive.Root, {
-  display: 'inline-flex',
-  backgroundColor: 'var(--color-panel-solid)',
-  borderRadius: 'var(--radius-3)',
-  boxShadow: '0 0 0 1px var(--gray-a3), var(--shadow-4)',
-  margin: 1,
-});
-
-const StyledItem = styled(ToggleGroupPrimitive.Item, {
-  all: 'unset',
-  color: 'var(--gray-a11)',
-  height: 35,
-  width: 35,
-  display: 'flex',
-  fontSize: 15,
-  lineHeight: 1,
-  alignItems: 'center',
-  justifyContent: 'center',
-  borderRight: '1px solid var(--gray-5)',
-  '&:first-child': {
-    marginLeft: 0,
-    borderTopLeftRadius: 'var(--radius-3)',
-    borderBottomLeftRadius: 'var(--radius-3)',
-  },
-  '&:last-child': {
-    borderRight: 0,
-    borderTopRightRadius: 'var(--radius-3)',
-    borderBottomRightRadius: 'var(--radius-3)',
-  },
-  '&:hover': {
-    backgroundColor: 'var(--gray-a2)',
-  },
-  '&[data-state=on]': {
-    backgroundColor: 'var(--gray-a3)',
-    color: 'var(--gray-12)',
-  },
-  '&:focus-visible': {
-    zIndex: 1,
-    boxShadow: '0 0 0 2px var(--accent-8)',
-  },
-});
-
-const ToggleGroup = StyledToggleGroup;
-const ToggleGroupItem = StyledItem;
+const ToggleGroup = styled(ToggleGroupPrimitive.Root, styles.ToggleGroup);
+const ToggleGroupItem = styled(ToggleGroupPrimitive.Item, styles.Item);
 
 export function PrimitivesHeroToggleGroup() {
   return (
