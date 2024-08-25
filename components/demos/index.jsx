5a7e4c1e2b09e4e1da8e9c94cdc6b3a9d60da5dd
Chance Strickland <hi@chance.dev>
2024-08-25 11:26:00 -0700

    Replace Stitches demo: `Progress`

--- components/demos/index.jsx
+++ components/demos/index.jsx
@@ -13,7 +13,7 @@ export { default as LabelDemo } from './Label/css-modules';
 export { default as MenubarDemo } from './Menubar/css-modules';
 export { default as NavigationMenuDemo } from './NavigationMenu/css-modules';
 export { default as PopoverDemo } from './Popover/css-modules';
-export { default as ProgressDemo } from './Progress/stitches';
+export { default as ProgressDemo } from './Progress/css-modules';
 export { default as RadioGroupDemo } from './RadioGroup/stitches';
 export { default as ScrollAreaDemo } from './ScrollArea/stitches';
 export { default as SelectDemo } from './Select/stitches';
