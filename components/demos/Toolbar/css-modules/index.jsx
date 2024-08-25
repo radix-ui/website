e5656888b98f25e388787a3e2dee6cf831b67c87
Chance Strickland <hi@chance.dev>
2024-08-25 11:34:32 -0700

    Replace Stitches demo: `Toolbar`

---
+++ components/demos/Toolbar/css-modules/index.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import * as Toolbar from '@radix-ui/react-toolbar';
+import {
+  StrikethroughIcon,
+  TextAlignLeftIcon,
+  TextAlignCenterIcon,
+  TextAlignRightIcon,
+  FontBoldIcon,
+  FontItalicIcon,
+} from '@radix-ui/react-icons';
+import styles from './styles.module.css';
+
+const ToolbarDemo = () => (
+  <Toolbar.Root className={styles.Root} aria-label="Formatting options">
+    <Toolbar.ToggleGroup type="multiple" aria-label="Text formatting">
+      <Toolbar.ToggleItem className={styles.ToggleItem} value="bold" aria-label="Bold">
+        <FontBoldIcon />
+      </Toolbar.ToggleItem>
+      <Toolbar.ToggleItem className={styles.ToggleItem} value="italic" aria-label="Italic">
+        <FontItalicIcon />
+      </Toolbar.ToggleItem>
+      <Toolbar.ToggleItem
+        className={styles.ToggleItem}
+        value="strikethrough"
+        aria-label="Strike through"
+      >
+        <StrikethroughIcon />
+      </Toolbar.ToggleItem>
+    </Toolbar.ToggleGroup>
+    <Toolbar.Separator className={styles.Separator} />
+    <Toolbar.ToggleGroup type="single" defaultValue="center" aria-label="Text alignment">
+      <Toolbar.ToggleItem className={styles.ToggleItem} value="left" aria-label="Left aligned">
+        <TextAlignLeftIcon />
+      </Toolbar.ToggleItem>
+      <Toolbar.ToggleItem className={styles.ToggleItem} value="center" aria-label="Center aligned">
+        <TextAlignCenterIcon />
+      </Toolbar.ToggleItem>
+      <Toolbar.ToggleItem className={styles.ToggleItem} value="right" aria-label="Right aligned">
+        <TextAlignRightIcon />
+      </Toolbar.ToggleItem>
+    </Toolbar.ToggleGroup>
+    <Toolbar.Separator className={styles.Separator} />
+    <Toolbar.Link className={styles.Link} href="#" target="_blank" style={{ marginRight: 10 }}>
+      Edited 2 hours ago
+    </Toolbar.Link>
+    <Toolbar.Button className={styles.Button} style={{ marginLeft: 'auto' }}>
+      Share
+    </Toolbar.Button>
+  </Toolbar.Root>
+);
+
+export default ToolbarDemo;
