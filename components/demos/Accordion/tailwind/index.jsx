1dfaaac5b02538b8dacfb09394ca1419906e5d01
Andrew Scofield <andrew@beldenstreet.com>
2024-10-08 10:21:13 -0500

    Fix Accodion demo in docs

--- components/demos/Accordion/tailwind/index.jsx
+++ components/demos/Accordion/tailwind/index.jsx
@@ -10,27 +10,27 @@ const AccordionDemo = () => (
 		defaultValue="item-1"
 		collapsible
 	>
-		<AccordionItem value="item-1">
-			<AccordionTrigger>Is it accessible?</AccordionTrigger>
-			<AccordionContent>
+		<Accordion.Item value="item-1">
+			<Accordion.Trigger>Is it accessible?</Accordion.Trigger>
+			<Accordion.Content>
 				Yes. It adheres to the WAI-ARIA design pattern.
-			</AccordionContent>
-		</AccordionItem>
+			</Accordion.Content>
+		</Accordion.Item>
 
-		<AccordionItem value="item-2">
-			<AccordionTrigger>Is it unstyled?</AccordionTrigger>
-			<AccordionContent>
+		<Accordion.Item value="item-2">
+			<Accordion.Trigger>Is it unstyled?</Accordion.Trigger>
+			<Accordion.Content>
 				Yes. It's unstyled by default, giving you freedom over the look and
 				feel.
-			</AccordionContent>
-		</AccordionItem>
+			</Accordion.Content>
+		</Accordion.Item>
 
-		<AccordionItem value="item-3">
-			<AccordionTrigger>Can it be animated?</AccordionTrigger>
-			<AccordionContent>
+		<Accordion.Item value="item-3">
+			<Accordion.Trigger>Can it be animated?</Accordion.Trigger>
+			<Accordion.Content>
 				Yes! You can animate the Accordion with CSS or JavaScript.
-			</AccordionContent>
-		</AccordionItem>
+			</Accordion.Content>
+		</Accordion.Item>
 	</Accordion.Root>
 );
 
