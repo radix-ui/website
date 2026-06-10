272dcf523d17c2c91b7fb089cbf1059164f099f1
Chance Strickland <hi@chance.dev>
2026-06-10 09:51:44 -0700

    migrate themes examples

---
+++ app/themes/(marketing)/example-dashboard/page.tsx
@@ -0,0 +1,48 @@
+import * as React from "react";
+import { Flex, Heading, Separator, Theme, Link, Box } from "@radix-ui/themes";
+import { ExampleThemesDashboard } from "@components/ExampleThemesDashboard";
+import { ExternalLinkIcon } from "@radix-ui/react-icons";
+import { baseMetadata } from "@utils/metadata";
+import { Metadata, Viewport } from "next";
+
+export const metadata: Metadata = {
+	...baseMetadata,
+	title: "Dashboard Example Layout – Radix Themes",
+};
+
+export const viewport: Viewport = {
+	width: "device-width",
+};
+
+export default function ExamplePage() {
+	return (
+		<Theme
+			accentColor="indigo"
+			grayColor="mauve"
+			className="radix-themes-default-fonts"
+		>
+			<Box p="9" minWidth="fit-content">
+				<Box mx="auto" width="fit-content">
+					<Flex align="center" direction="column">
+						<Heading align="center" mb="2">
+							Example layout built with Radix Themes
+						</Heading>
+						<Flex asChild align="center" gap="6px">
+							<Link
+								size="2"
+								color="gray"
+								target="_blank"
+								href="https://github.com/radix-ui/website/blob/main/components/ExampleThemesDashboard.tsx"
+							>
+								Source code
+								<ExternalLinkIcon />
+							</Link>
+						</Flex>
+						<Separator size="2" my="8" />
+					</Flex>
+					<ExampleThemesDashboard />
+				</Box>
+			</Box>
+		</Theme>
+	);
+}
